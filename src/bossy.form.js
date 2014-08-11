867d78971cc9519c9c717fef09ec52bddd0d08cc
Geoff Lawson <geoff.lawson@build.com>
2014-08-10 17:27:48 -0700

    #9: Moved schema and data into util services.  Added gulp build to package modules.

---
+++ src/bossy.form.js
@@ -0,0 +1,70 @@
+angular.module('bossy.form', [])
+    .directive('bossyForm', function ($compile, $http, $schema, $data) {
+        var _schema,
+            _data,
+            _itemTemplate = {
+                number: function () {
+                    return '<input type="number" />'
+                },
+                text: function () {
+                    return '<input type="text" />'
+                },
+                textArea: function () {
+                    return '<textarea></textarea>'
+                }
+            };
+
+        function setData(data) {
+            _data = $data.getData(data);
+        }
+
+        function setSchema(schema) {
+            _schema = $schema.getSchema(schema);
+        }
+
+        function buildTemplate(schemaPart, parentKey) {
+            var template = '',
+                fullKey = '';
+
+            angular.forEach(schemaPart, function(value, key) {
+                if (value.type) {
+                    switch (value.type) {
+                        case 'object':
+                            console.log(fullKey + ' is object');
+                            template += buildTemplate(value.properties, fullKey);
+                            break;
+                        case 'array':
+                            console.log(fullKey + ' is array');
+                            template += buildTemplate(value.items.properties, fullKey);
+                            break;
+                        case 'number' || 'integer':
+                            console.log(fullKey + ' is number or integer');
+                            template += _itemTemplate['number']();
+                            break;
+                        case 'string':
+                            console.log(fullKey + ' is string');
+                            template += _itemTemplate['text']();
+                            break;
+                    }
+                }
+            });
+
+            return template;
+        }
+
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '',
+            link: function (scope, element, attributes) {
+
+                setData(scope.options.data);
+                setSchema(scope.options.schema);
+
+                element.html(buildTemplate(_schema));
+                $compile(element.contents())(scope);
+            }
+        };
+
+    })
+;
---
+++ src/bossy.schema.js
@@ -0,0 +1,42 @@
+angular.module('bossy.schema', [])
+    .factory('$schema', function () {
+
+        function _getSchema (schema) {
+            if (angular.isString(schema)) {
+                return _getRemoteSchema(schema);
+            }
+            else if (angular.isObject(schema)) {
+                return schema;
+            }
+            else {
+                //TODO: replace error message with online doc link like ng errors
+                console.error('directive.bossyForm: no schema url or object given');
+            }
+        }
+
+        function _getRemoteSchema() {
+            var deferred = $q.defer();
+
+            $http.get( schema )
+                .success( function( data ) {
+                    if( angular.isObject( data ) ) {
+                        deferred.resolve(data);;
+                    }
+                    else {
+                        //TODO: replace error message with online doc link like ng errors
+                        deferred.reject('directive.bossyForm: GET request to url did not produce schema object');
+                    }
+                })
+                .error( function(data, status) {
+                    //TODO: replace error message with online doc link like ng errors
+                    deferred.reject('directive.bossyForm: GET request to url "' + schema + '" failed with status "' + status + '"');
+                });
+
+            return deferred.promise;
+        }
+
+        return {
+            getSchema: _getSchema
+        }
+    })
+;
