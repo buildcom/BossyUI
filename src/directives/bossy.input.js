1f2c6ae47d54d4e030b85ce37e0dcb6f2a28090a
Tauseef <tauseef.jamadar@yahoo.com>
2014-09-03 21:58:45 -0700

    Adding bossy controllers for additional event handling.

--- src/directives/bossy.input.js
+++ src/directives/bossy.input.js
@@ -1,12 +1,18 @@
 angular.module('app.directive.bossy.input', [])
     .run(function($templateCache){
-        $templateCache.put('bossy-input.html', '<div class="form-group bossy-input"><label for="">{{title}}</label><input type="{{type}}" class="form-control" placeholder="" value="{{value}}"/><span></span></div>');
+        $templateCache.put('bossy-input.html', 
+        	'<div ng-controller="bossyInputController" class="form-group bossy-input"><label for="">{{title}}</label><input ng-model="value" ng-change="onChange(value)" type="{{type}}" class="form-control" placeholder="" value="{{value}}"/><span></span></div>');
     })
+    .controller('bossyInputController',['$scope', function($scope){
+    	$scope.onChange = function(elemValue){
+    		//console.log("changed value",elemValue);
+    		// Add additional event handling and error handling.
+    	};
+    }])
     .directive('bossyInput', function ($compile, $http, $schema, $data, $templateCache) {
-    	console.log($templateCache.get('bossy-input.html'));
     	return {
 			restrict: "E",
-			replace: true,
+			require: 'ngModel',
 			scope: {
 				title: "=",
 				value: "=",
