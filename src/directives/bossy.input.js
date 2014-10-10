158ae3f3d7265a0b5a122f70afe857dc5af140ff
Daniel Sluis <daniel@lulus.com>
2014-10-10 14:03:16 -0700

    updating directive calls to minification-ready format

--- src/directives/bossy.input.js
+++ src/directives/bossy.input.js
@@ -2,7 +2,7 @@ angular.module('app.directive.bossy.input', [])
     .run(function($templateCache){
         $templateCache.put('bossy-input.html', '<div class="form-group bossy-input"><label for="">{{title}}</label><input type="{{type}}" class="form-control" placeholder="" value="{{value}}"/><span></span></div>');
     })
-    .directive('bossyInput', function ($compile, $http, $schema, $data, $templateCache) {
+    .directive('bossyInput',['$compile','$http','$schema','$data','$templateCache', function ($compile, $http, $schema, $data, $templateCache) {
     	return {
 			restrict: "E",
 			replace: true,
@@ -14,4 +14,4 @@ angular.module('app.directive.bossy.input', [])
 			},
 			template: $templateCache.get('bossy-input.html')
 		};
-    });
+    }]);
