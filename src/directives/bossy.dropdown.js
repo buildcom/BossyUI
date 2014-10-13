8c43602ff2ad2c29caf222a7808349b768a470f2
AlmondSeason <almondseason@gmail.com>
2014-10-12 17:26:36 -0700

    Fixes dropdown
    For both directive embedded and separate controller

--- src/directives/bossy.dropdown.js
+++ src/directives/bossy.dropdown.js
@@ -3,20 +3,22 @@ angular.module('bossy.dropdown', [])
 		$templateCache.put('jasmineTest.html', 'jasmineTest.html');
 	})
 	
-	.factory('bossyDropdownFactory', ['$http', function($http) {
+	.factory('bossyDropdownFactory', ['$http', /*'$data',*/ function($http /*$data*/) {
 		var dataArray = [];
+		
+		var fill = function(){
 		$http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json')
-		.success(function(data){
-			dataArray = data;
-			console.log(dataArray[0].name);
-
-		})
-		.error(function(data) {
-			console.log("http.get FAILED");
-			$scope.data = data || "Request failed";
-		 })
+			.success(function(data){
+				dataArray = data;
+				console.log(dataArray[0].name);
+			})
+			.error(function(data) {
+				console.log("http.get FAILED");
+				$scope.data = data || "Request failed";
+			})
+		}
 		//TEST ARRAY 
-		 // var dataArray =  [
+		 // dataArray =  [
         // {id: 0, name:'AlenMania'},
         // {id: 1, name:'Albania'},
         // {id: 2, name:'Algeria'},
@@ -38,21 +40,25 @@ angular.module('bossy.dropdown', [])
 			replace: true,
 			templateUrl: 'bossy.dropdown.html',
 			controller:['$scope', 'bossyDropdownFactory', function($scope, bossyDropdownFactory) {
-					this._options = {
+				this._options = {
 					titles: bossyDropdownFactory._options.titles,
 					contents: bossyDropdownFactory._options.contents
 				};
-					$scope.myVal = this._options.contents;
+					$scope.myVals = this._options.contents;
 			}],
 			controllerAs: "drops"		
 		};
 	})
 	
 	.controller('bossyDropdownCtrl', function($compile, $http, bossyDropdownFactory, $scope /*$data,*/ /*$schema*/) {
-		this._options = {
+		$scope._options = {
 			titles: bossyDropdownFactory._options.titles,
 			contents: bossyDropdownFactory._options.contents
 		};
+		// $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json')
+			// .success(function(data){
+				// $scope._options.contents = data;
+			// })
 	})
 	
 	
