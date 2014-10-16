961244068ba3bb15751f9091e96e48bc388ef995
AlmondSeason <almondseason@gmail.com>
2014-10-15 22:11:35 -0700

    Adds Abbreviation Functionality to Directive Drop
    Adds Abbreviation Functionality to Directive Drop
    All essential changes in bossy.dropdown.html

--- src/directives/bossy.dropdown.js
+++ src/directives/bossy.dropdown.js
@@ -3,20 +3,10 @@ angular.module('bossy.dropdown', [])
 		$templateCache.put('jasmineTest.html', 'jasmineTest.html');
 	})
 	
-	.factory('bossyDropdownFactory', ['$http', /*'$data',*/ function($http /*$data*/) {
-		
-		return $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json');
-			
-		//TEST ARRAY 
-		 // dataArray =  [
-        // {id: 0, name:'AlenMania'},
-        // {id: 1, name:'Albania'},
-        // {id: 2, name:'Algeria'},
-        // {id: 3, name:'Andorra'},
-        // {id: 4, name:'Angola'},
-        // {id: 5, name:'Antigua & Deps'}
-		// ];
-	}])
+	.factory('bossyDropdownFactory', function($http /*$data*/) {
+		var promise = $http.get('https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json');
+		return promise;
+	})
 	
 	.directive('bossyDropdown', function($compile, $http /*$data,*/ /*$schema*/) {
 		return {
@@ -25,8 +15,9 @@ angular.module('bossy.dropdown', [])
 			templateUrl: 'bossy.dropdown.html',
 			controller: function($scope, bossyDropdownFactory) {
 				$scope.contents = [];
-				
-				bossyDropdownFactory.success(function(data){
+
+				bossyDropdownFactory
+					.success(function(data){
 						$scope.contents = data;
 					})
 					.error(function(data) {
@@ -38,15 +29,16 @@ angular.module('bossy.dropdown', [])
 		};
 	})
 	
-	.controller('bossyDropdownCtrl', function($compile, $http, bossyDropdownFactory, $scope /*$data,*/ /*$schema*/) {
-			$scope.contents = [];
-			
-			bossyDropdownFactory.success(function(data){
-				$scope.contents = data;
+	.controller('bossyDropdownCtrl', function($http, bossyDropdownFactory, $scope /*$data,*/ /*$schema*/) {
+		$scope.items = [];
+		
+		bossyDropdownFactory
+			.success(function(data){
+				$scope.items = data;
 			})
 			.error(function(data) {
 				console.log("http.get FAILED");
-				$scope.contents = data || "Request failed";
+				$scope.items = data || "Request failed";
 			})
 	})
 	
