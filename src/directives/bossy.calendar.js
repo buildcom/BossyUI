ff4db175987c948e30bf80adb9221ee8cf8d530a
Jason Merino <jasonmerino@gmail.com>
2014-10-30 22:22:29 -0700

    bossy.calendar - Adds ability to give start and end date.

--- src/directives/bossy.calendar.js
+++ src/directives/bossy.calendar.js
@@ -2,6 +2,9 @@ angular.module('bossy.calendar', [])
 	.controller('CalendarController', ['$scope', function ($scope) {
 
 		var _monthMaps = {},
+			options = {},
+			defaults = {
+			},
 			universal = {
 				DAY: 24 * 60 * 60 * 1000,
 				HOUR: 60 * 60 * 1000
@@ -31,33 +34,60 @@ angular.module('bossy.calendar', [])
 			'December'
 		];
 
-		function _getTimeObject(date) {
+		function getStandardTime(date) {
 			return {
 				raw: date,
 				year: date.getFullYear(),
 				monthName: _getMonthName(date.getMonth()),
 				month: date.getMonth(),
-				day: _getDay(date),
+				day: _getDayName(date),
 				date: date.getDate(),
 				time: date.getTime()
 			};
 		}
 
+		function _getTimeObjectIfDate(date) {
+			if (angular.isDate(new Date(date))) {
+				return getStandardTime(new Date(date));
+			}
+			return false;
+		}
+		function _setConfigOptions() {
+			try {
+				var config = JSON.parse($scope.config);
+				config.start = _getTimeObjectIfDate(config.start);
+				config.end = _getTimeObjectIfDate(config.end);
+				options = angular.extend({}, defaults, config);
+			} catch (error) {
+				console.error(new Error(error));
+			}
+		}
+
+		function _dayIsOutOfRange(_date) {
+			if (options.start && options.end && (_date.time < options.start.time || _date.time > options.end.time)) {
+				return true;
+			} else if (options.start && _date.time < options.start.time) {
+				return true;
+			} else if (options.end && _date.time > options.end.time) {
+				return true;
+			}
+		}
+
 		function _setSelectedDate(date) {
-			$scope.selected = _getTimeObject(date);
+			$scope.selected = getStandardTime(date);
 			$scope.ngModel = $scope.selected.raw;
 		}
 
 		function _setCurrentMonthAndYear(month, year) {
 			var date = new Date(year !== undefined ? year : $scope.selected.year, month !== undefined ? month : $scope.selected.month, 1);
-			$scope.current = _getTimeObject(date);
+			$scope.current = getStandardTime(date);
 		}
 
 		function _getMonthName(month) {
 			return $scope.months[month];
 		}
 
-		function _getDay(date) {
+		function _getDayName(date) {
 			return $scope.days[date.getDay()];
 		}
 
@@ -74,7 +104,10 @@ angular.module('bossy.calendar', [])
 		};
 
 		$scope.selectDate = function(time) {
-			var date = _getTimeObject(new Date(time));
+			var date = getStandardTime(new Date(time));
+			if (_dayIsOutOfRange(date)) {
+				return;
+			}
 			if (date.month !== $scope.current.month) {
 				_setCurrentMonthAndYear(date.month, date.year);
 				$scope.updateDateMap();
@@ -100,8 +133,9 @@ angular.module('bossy.calendar', [])
 					} else if (_thisDate.getHours() === 1) {
 						_thisDate = (new Date(_thisDate.getTime() - universal.HOUR));
 					}
-					var _date = _getTimeObject(_thisDate);
-					_date.bold = _thisDate.getMonth() === $scope.current.raw.getMonth();
+					var _date = getStandardTime(_thisDate);
+					_date.dayInMonth = _thisDate.getMonth() === $scope.current.raw.getMonth() ? 'day-in-month' : '';
+					_date.disabledDay = _dayIsOutOfRange(_date) ? 'disabled-day' : '';
 					week.push(_date);
 				}
 				firstWeekDay = new Date(firstWeekDay.getTime() + (7 * universal.DAY));
@@ -109,7 +143,7 @@ angular.module('bossy.calendar', [])
 			}
 		};
 
-		// init to current date
+		_setConfigOptions();
 		_setSelectedDate($scope.ngModel || new Date());
 		_setCurrentMonthAndYear();
 		$scope.updateDateMap();
@@ -118,9 +152,10 @@ angular.module('bossy.calendar', [])
 		return {
 			restrict: 'AE',
 			scope: {
-				ngModel: '='
+				ngModel: '=',
+				config: '@'
 			},
-			template: '<table><tr><td ng-click="previousMonth()">&lt;</td><td colspan="5">{{current.monthName}} {{current.year}}</td><td ng-click="nextMonth()">&gt;</td></tr><td ng-repeat="day in days" title="{{day}}">{{day | limitTo : 2}}</td><tr ng-repeat="week in dateMap"><td ng-repeat="current in week" ng-click="selectDate(current.time)"><b ng-if="current.bold">{{current.date}}</b><span ng-if="!current.bold">{{current.date}}</span></td></tr><tr><td colspan="7">{{selected.day}}, {{selected.monthName}} {{selected.date}}, {{selected.year}}</td></tr></table>',
+			template: '<style>bossy-calendar .day-in-month{font-weight:700}bossy-calendar .disabled-day{color:#ccc}</style><table><tr><td ng-click="previousMonth()" title="Previous month">&lt;</td><td colspan="5">{{current.monthName}} {{current.year}}</td><td ng-click="nextMonth()" title="Next month">&gt;</td></tr><td ng-repeat="day in days" title="{{day}}">{{day | limitTo : 2}}</td><tr ng-repeat="week in dateMap"><td ng-repeat="current in week" ng-click="selectDate(current.time)" class="{{current.dayInMonth}} {{current.disabledDay}}">{{current.date}}</td></tr><tr><td colspan="7">{{selected.day}}, {{selected.monthName}} {{selected.date}}, {{selected.year}}</td></tr></table>',
 			controller: 'CalendarController'
 		};
 	}]);
