d1189dff682528ff85725ee4cb46c93565ff1e4f
Shelby Martin <smartin43@mail.csuchico.edu>
2014-12-07 19:24:50 -0800

    Updated dropdown and tooltips with error checking.

--- src/directives/bossy.dropdown.js
+++ src/directives/bossy.dropdown.js
@@ -1,10 +1,14 @@
 angular.module('bossy.dropdown', [])
+	.run(function($templateCache){
+        $templateCache.put('bossy-dropdown.html', '<div><select ng-options="item[dropdown.title] for item in dropdown.items | orderBy: dropdown.title" ng-model="selectedItem" ng-change="dropdown.updateSelectedItem(selectedItem)"><option value="" ng-hide="selectedItem">Please select one...</option></select></div>');	
+    })
 	.directive('bossyDropdown', function($http, $compile) {
 		return {
 			restrict: 'EA',
 			scope: {
 				config: "=",
-				select: "="
+				select: "=",
+				items: "="
 			},
 			templateUrl: '',
 			link: function(scope, element, attrs) {
@@ -25,32 +29,48 @@ angular.module('bossy.dropdown', [])
 				thisDropdown.title = $scope.config.title;
 				thisDropdown.items = [];
 
-				//Retrieve json containing objects to populate the dropdown
-				$http.get($scope.config.src)
-					.success(function(data) {
-						thisDropdown.items = data;
-						//Attaches retrieved items to $scope.config for additional functionality
-						if($scope.config.items)
-							$scope.config.items = thisDropdown.items;
-					})
-					.error(function(data) {
-						console.log("http.get FAILED");
-						$scope.config.items = data || "Request failed";
-					});
-
-				//Function called to update select via ng-change in the template
-				thisDropdown.updateSelectedItem = function(selectedItem){
-					//selectedItem attached to two different selects for multifunctionality
-					//user can collect and utilize multiple select objects if passing in a select param
-					$scope.select = selectedItem;
-					$scope.config.select = selectedItem;
+				//Retrieve json containing objects to populate the dropdown.
+				if($scope.config.src) {
+					//Checks that config.src is a JSON file.
+					if($scope.config.src.substr($scope.config.src.length-5, $scope.config.src.length) == '.json') {
+						$http.get($scope.config.src)
+							.success(function(data) {
+								thisDropdown.items = data;
+								//Checks validity of the title field as it applies to the JSON.
+								if(!thisDropdown.items[0].hasOwnProperty(thisDropdown.title))
+									console.error("ERROR: $scope.config.title: \'" + $scope.config.title + "\'' is not a member of the loaded JSON data. Please specify a valid \'title\' to list.");
+								//Attaches retrieved items to $scope.items for additional functionality.
+								if($scope.items)
+									$scope.items = thisDropdown.items;
+							})
+							.error(function(data) {
+								console.error("ERROR: Fail to load JSON data from the path: \'" + $scope.config.src + "\'");
+							});
+					}
+					//Logs an error to identify that a json file was not loaded.
+					else {
+						console.error( "ERROR: \'$scope.config.src\': \'" + $scope.config.src + "\' is not a valid JSON file.");
+					}
+					//Function called to update select in the template.
+					thisDropdown.updateSelectedItem = function(selectedItem) {
+						//Single select object tied to the config object.
+						if($scope.config.select)
+							$scope.config.select = selectedItem;
+						//User can collect and utilize multiple select objects with the same config object if passing in a distinct select param.
+						if($scope.select)
+							$scope.select = selectedItem;
+					};
+					//Determine if custom template Url has been defined.
+					if($scope.config.template)
+						thisDropdown.template = $scope.config.template;
+					else {
+						thisDropdown.template = 'bossy-dropdown.html';
+					}
+				}
+				//Logs an error if 'src' has not been defined.
+				else {
+					console.error( "ERROR: \'$scope.config.src\' has not been specified within the \'config\' object. Please pass in a valid path to a JSON file.");
 				};
-
-				//Determine if custom template Url has been defined.
-				if($scope.config.template)
-					thisDropdown.template = $scope.config.template;
-				else
-					thisDropdown.template = 'bossy-dropdown.html';
 			},
 			controllerAs: 'dropdown'
 		};
--- test/directives/bossy.dropdown.spec.js
+++ test/directives/bossy.dropdown.spec.js
@@ -1,59 +1,52 @@
-describe("bossyDropdown", function () {
-var $rootScope,
-	$scope,
-	controller;
+describe('bossyDropdown',function() {
+    var createCtrl,element;
 
-	beforeEach(function() {
-		angular.module('whatever', ['bossy.dropdown'])
-				.controller('testController', ['$scope', function($scope) {
-					$scope.myConfig = {
-						src: 'https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json',
-						title: 'abbreviation',
-						items: [],
-						select: {}//,
-						//template: '<div>shit</div>'
-						//template: 'other-template.html'
-						//template: 'https://raw.githubusercontent.com/smartin43/javabang-BossyUI/smartin43/src/directives/templates/other-template.html'
-					};
-				}]);
 
-		inject(function ($injector) {
-			$rootScope = $injector.get('$rootScope');
-			$scope = $rootScope.$new();
-			controller = $injector.get('controller')("testController", {$scope: $scope});
-		});
-	});
+    beforeEach(module('Templates'));
+    beforeEach(inject(function($controller,$http,$compile,$rootScope){
 
-	describe()
+         $rootScope.myConfig={
+            src: 'https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json',
+            title: 'abbreviation',
+            select: {},
+            // items: true,
+            // template: '<div>shit</div>'
+            template: 'temp2.html'
+
+        };
+        $rootScope.selectItem = "abc";
+
+        element = angular.element('<div  bossy-dropdown config={{myConfig}} select="{{selectItem}}"></div>');
+        $compile(element)($rootScope);
+        $rootScope.$digest();
+
+
+    }));
+
+    it('should not be null',function(){
+
+        expect(element).not.toBe(null);
+    })
+
+    it('should correctly pass the config Object',function(){
+
+       var myConfig={
+            src: 'https://gist.githubusercontent.com/mshafrir/2646763/raw/bfb35f17bc5d5f86ec0f10b80f7b80e823e9197f/states_titlecase.json',
+            title: 'abbreviation',
+            select: {},
+            template: 'temp2.html'
+
+        };
+
+        expect(element.attr("config")).toMatch(myConfig);
+
+
+    })
+
+    it('should have correct selected item',function(){
+
+        expect(element.attr("select")).toMatch('abc');
+    });
+
+
 });
-
-
-describe("bossyDropdown",function(){
-var element;
-var scope;
-var compile;
-var val;
-var http;
-
-
-beforeEach(module('Templates'))
-beforeEach(inject(function($compile,$rootScope, $http){
-	scope=$rootScope;
-	compile  = $compile;
-	val = true;
-	element = angular.element('<bossy-dropdown title="name"></bossy-dropdown>');
-	$compile(element)(scope);
-	$rootScope.$digest();
-	http = $http;
-
-}));
-	it('should populate bossy dropdown',function(){
-		expect(element.attr("title")).toMatch("name");
-
-	})
-	it('should throw error to broken promise',function(){
-		expect(http.get("")).toThrow(new Error("http.get FAILED"));
-
-	})
-
- })
