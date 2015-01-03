e159d966ba3da5e2a217743752a7f8378d669de0
Tuhin <tuhintshukla@gmail.com>
2015-01-03 13:01:40 -0800

    Added unit tests for Calendar widget

--- test/directives/bossy.calendar.spec.js
+++ test/directives/bossy.calendar.spec.js
@@ -1,31 +1,49 @@
-describe('BossyCalenderUnitTests', function() {
-    // Load the module with MainController
-   beforeEach(module('bossy.calendar'));
+describe('CalendarControllerUnitTests', function() {
 
-    var ctrl, scope,element;
+    beforeEach(module('bossy.calendar'));
 
-
+    var ctrl, scope;
     // inject the $controller and $rootScope services
     // in the beforeEach block
-    beforeEach(inject(function ($controller, $rootScope) {
+    beforeEach(inject(function($controller, $rootScope) {
         // Create a new scope that's a child of the $rootScope
         scope = $rootScope.$new();
+        scope.config={};
         // Create the controller
         ctrl = $controller('CalendarController', {
             $scope: scope
         });
-        scope.dirConfig={};
-
-        element = angular.element('<bossy-calendar config="scope.dirConfig"></bossy-calendar>');
-
     }));
+    describe('Controller Initialization',function(){
 
+        it('initialization variables should not be null',function(){
 
-    it('should work',function(){
+            expect(scope.months).toNotBe(undefined);
+            expect(scope.days).toNotBe(undefined);
 
-       console.log("done");
-        console.log(scope);
+        });
+        it('updateDateMap should be called', function() {
 
-    })
+            spyOn(scope,"updateDateMap");
+            //console.log(ctrl);
+            //console.log(scope);
+            scope.updateDateMap();
+            expect(scope.updateDateMap).toHaveBeenCalled();
+        });
+
+        it('scope.nextMonth should be called',function(){
+
+            spyOn(scope,'nextMonth');
+            scope.nextMonth();
+            expect(scope.nextMonth).toHaveBeenCalled();
+        });
+
+        it('scope.previousMonth should be called',function(){
+
+            spyOn(scope,'previousMonth');
+            scope.previousMonth();
+            expect(scope.previousMonth).toHaveBeenCalled();
+        });
+    });
 
 });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -71,8 +71,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-
-    // Continuous Integration mode
+      // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
