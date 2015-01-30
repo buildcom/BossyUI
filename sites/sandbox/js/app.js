91c342993926e9e7392141aab74d8539eb197c8f
Geoff Lawson <geoff.lawson@build.com>
2015-01-29 16:04:50 -0800

    SANDBOX: WIP

--- sites/sandbox/js/app.js
+++ sites/sandbox/js/app.js
@@ -1,6 +1,23 @@
 angular.module('SandboxApp', [
+    'ui.router',
     'bossy'
 ])
+
+    .config(['$stateProvider', function($stateProvider) {
+        var module =  angular.module('bossy');
+
+        angular.forEach(module.requires, function(directive) {
+            var name = directive.split('.')[1];
+
+            $stateProvider
+                .state(name, {
+                    url: '/' + name,
+                    templateUrl: function () {
+                        return '/docs/bossy.' +name + '.html';
+                    }
+                });
+        });
+    }])
 
     .controller('DemoCtrl', ['$scope', '$document', '$window', '$compile', function($scope, $document, $window, $compile) {
         var module =  angular.module('bossy');
@@ -8,20 +25,19 @@ angular.module('SandboxApp', [
         $scope.directives = [];
         $scope.directiveConfig = {};
 
-        for (var i in module.requires) {
-            $scope.directives.push(module.requires[i]);
-        }
+        angular.forEach(module.requires, function(directive) {
+            $scope.directives.push(directive.split('.')[1]);
+            //$scope.states.push(directive.split('.')[1]);
+        });
 
-        $scope.developLocally = function(directive) {
-            var demo;
-            var html;
-
-            demo = angular.element(document.getElementById('demo'));
-            html = '<' + directive.replace('.', '-') + ' config="directiveConfig">';
-
-            $compile(demo.html(html))($scope);
-
-        };
-
+        //$scope.developLocally = function(directive) {
+        //    var demo,
+        //        html;
+        //
+        //    demo = angular.element(document.getElementById('results'));
+        //    html = '<' + directive.replace('.', '-') + ' config="directiveConfig">';
+        //
+        //    $compile(demo.html(html))($scope);
+        //};
     }])
 ;
