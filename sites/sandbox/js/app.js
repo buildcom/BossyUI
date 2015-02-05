85f9d962f1acc1495c74a178c28761d1656d0ae7
Tauseef <tauseef.jamadar@yahoo.com>
2015-02-04 21:16:59 -0800

    Adding angular controller to the editor.

--- sites/sandbox/js/app.js
+++ sites/sandbox/js/app.js
@@ -1,131 +1,47 @@
 angular.module('SandboxApp', [
-    'bossy',
-    'mc.resizer'
+    'ui.router',
+    'bossy'
 ])
-.controller('SandboxCtrl', ['$scope', '$document', '$window', function($scope, $document, $window) {
-    var module =  angular.module('bossy'),
-        bossyRef = $window.location.origin + $window.location.pathname.replace('index.html', '') + 'js/bossy.all.js';
 
-    $scope.directives = [];
+    .config(['$stateProvider', function($stateProvider) {
+        var module =  angular.module('bossy');
 
-    for (var i in module.requires) {
-        $scope.directives.push(module.requires[i]);
-    }
+        angular.forEach(module.requires, function(directive) {
+            var name = directive.split('.')[1];
 
-    // PostToCodepen
-    $scope.postToCodepen = function (directive) {
-        var url = 'http://codepen.io/pen/define',
-            penForm = angular.element('<form style="display: none;" target="_blank" method="post" action="' + url + '"></form>'),
-            penFormData = angular.element('<input type="hidden" name="data">');
+            $stateProvider
+                .state(name, {
+                    url: '/' + name,
+                    templateUrl: function () {
+                        return '/docs/bossy.' +name + '.html';
+                    }
+                });
+        });
+    }])
 
-        // Replace Quotes and Return object as string.
-        var setDataString = function (data){
-            var string = JSON.stringify(data);
-            string = string.replace(/"/g, "&quot;");
-            string = string.replace(/'/g, "&apos;");
-            return string;
-        };
+    .controller('DemoCtrl', ['$scope', '$document', '$window', '$compile', function($scope, $document, $window, $compile) {
+        var module =  angular.module('bossy');
 
+        $scope.directives = [];
+        $scope.directiveConfig = {};
 
-        var getData = function(){
-            var htmlCode = '<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.1/angular.min.js"></script>' +
-                '<script src="' + bossyRef + '"></script>\n' +
-                '<div ng-app="sandboxApp" ng-controller="sandboxCtrl">\n' +
-                '   <' + directive.replace('.', '-') + ' config="directiveConfig">\n' +
-                '</div>';
+        angular.forEach(module.requires, function(directive) {
+            $scope.directives.push(directive.split('.')[1]);
+            //$scope.states.push(directive.split('.')[1]);
+        });
 
-            var jsCode = '' +
-                'angular.module("sandboxApp", ["bossy"])\n\n' +
-                '.controller("sandboxCtrl", ["$scope", function($scope) {\n' +
-                '' +
-                '   $scope.directiveConfig = {};\n' +
-                '}])\n\n;';
-
-            return setDataString({
-                title              : "",
-                description        : "",
-                html               : htmlCode,
-                html_pre_processor : "none",
-                css                : "",
-                css_pre_processor  : "scss",
-                css_starter        : "neither",
-                css_prefix_free    : false,
-                js                 : jsCode,
-                js_pre_processor   : "none",
-                js_modernizr       : false,
-                js_library         : "",
-                html_classes       : "",
-                css_external       : "",
-                js_external        : ""
-            });
-        };
-
-        penFormData.attr('value', getData());
-        penForm.append(penFormData);
-
-        $document.find('body').append(penForm);
-        penForm[0].submit();
-        penForm.remove();
-
-
-    };
-
-    $scope.openFiddle = function (directive) {
-        var url = 'http://jsfiddle.net/api/post/angularjs/1.2.1/',
-            form = angular.element('<form style="display: none;" target="_blank" method="post" action="' + url + '"></form>'),
-            js = angular.element('<input type="hidden" name="js">'),
-            html = angular.element('<input type="hidden" name="html">'),
-            resources = angular.element('<input type="hidden" name="resources">');
-
-
-        var htmlCode = '' +
-            '<script src="' + bossyRef + '"></script>\n' +
-            '<div ng-app="sandboxApp" ng-controller="sandboxCtrl">\n' +
-            '   <' + directive.replace('.', '-') + ' config="directiveConfig">\n' +
-            '</div>';
-
-        html.attr('value', htmlCode);
-        form.append(html);
-
-        var jsCode = '' +
-            'angular.module("sandboxApp", ["bossy"])\n\n' +
-            '.controller("sandboxCtrl", ["$scope", function($scope) {\n' +
-            '' +
-            '   $scope.directiveConfig = {};\n' +
-            '}])\n\n;';
-
-        js.attr('value', jsCode);
-        form.append(js);
-
-        resources.attr('value', '');
-        form.append(resources);
-
-        $document.find('body').append(form);
-        form[0].submit();
-        form.remove();
-    };
-    //$scope.developLocally = function(directive) {
-    //    var demo,
-    //        html;
-    //
-    //    demo = angular.element(document.getElementById('results'));
-    //    html = '<' + directive.replace('.', '-') + ' config="directiveConfig">';
-    //
-    //    $compile(demo.html(html))($scope);
-    //};
-}])
-.directive('doneTyping', function($rootScope){
-    
-    return {
-        restrict: 'A',
-        link: function(scope, element, atts) {
-            console.log("binding events to the textarea");
-            element.find('div.ace_line').bind('onchange', function(){
-                $rootScope.$broadcast('doneTyping');
-            });
-            scope.$on('doneTyping', function(event, message){
-                console.log("DONE TYPING");
-            });
-        }
-    };
-});
+        //$scope.developLocally = function(directive) {
+        //    var demo,
+        //        html;
+        //
+        //    demo = angular.element(document.getElementById('results'));
+        //    html = '<' + directive.replace('.', '-') + ' config="directiveConfig">';
+        //
+        //    $compile(demo.html(html))($scope);
+        //};
+    }])
+    .controller('EditorCtrl', ['$scope', '$document', '$window', '$compile', function($scope, $document, $window, $compile) {
+        var editor = angular.element(document.getElementById('editor-result'));
+        $compile(editor.html(editor.html()))($scope);
+    }])
+;
