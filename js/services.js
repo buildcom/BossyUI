7d0119cb8ce7bd156a85f9fd4ba573d6fa22eb67
Adrian_Miller <adrian.miller@build.com>
2015-02-16 11:20:10 -0800

    Updated bossy colors to brand

---
+++ js/services.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+// angular.module('bossyui.services', [])
+// 	app.factory ('logo', function ($http, $q) {
+// 		function NameService() {
+//       self.getName = function() {
+//          return $http.get('img/logo_horizontal.svg', fiddleResponse, fiddleHeaders);
+//       };
+//     }
+
+//     return new NameService();
+// 	});
