faf7c89b922038fe542ce2d3f59f4dc529c2de82
mymercurialsky <michael.gonsalves84@gmail.com>
2015-09-28 13:32:07 -0700

    Added demo code to tool tip to demonstrate class changes based on options.

--- src/directives/bossy.tooltip.js
+++ src/directives/bossy.tooltip.js
@@ -9,10 +9,17 @@ function Tooltip()
     restrict: 'A',
     scope: {
       data: '=',
+      options: '=',
     },
     link: function(scope, element, attrs){
       element.bind('mouseenter', function(){
         element.after('<span id="tooltip" class="btn btn-danger">' + scope.data.text+ '</span>');
+
+        if (scope.options.color === 'Blue'){
+        angular.element(document.querySelector('#tooltip')).removeClass('btn btn-danger').addClass('btn btn-primary');
+        angular.element(document.querySelector('#tooltip')).css('position', 'relative');
+        angular.element(document.querySelector('#tooltip')).css('bottom', '50px');
+        }
 
       });
 
