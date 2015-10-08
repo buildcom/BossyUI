ad4e005f388498c2648555d51f525405ed539cdc
mymercurialsky <michael.gonsalves84@gmail.com>
2015-10-07 18:44:19 -0700

    Added alert and download options to tooltip.

--- src/directives/bossy.tooltip.js
+++ src/directives/bossy.tooltip.js
@@ -13,54 +13,60 @@ function Tooltip()
     },
     link: function(scope, element, attrs){
 
-      // What do we do if no text is given?
-      if (!scope.data){
-        scope.data = {text:"Sample Tool Tip"};
-      }
+       // What do we do if no text is given?
+       if (!scope.data){
+         scope.data = {text:"Sample Tool Tip"};
+       }
 
-      // Fail safe in case options are not given
-      if (!scope.options){
-        scope.options = {};
-      }
+       // Fail safe in case options are not given
+       if (!scope.options){
+         scope.options = {};
+       }
 
-      // Determine class options
-      var tooltipClass = "tooltip-active";
+       // Determine class options
+       var tooltipClass = "tooltip-active";
 
-      // Alignment
-      if (scope.options.align === "Left"){
-        tooltipClass += " tooltip-left";
-      }
-      else if (scope.options.align === "Right"){
-        tooltipClass += " tooltip-right";
-      }
+       // Alignment
+       if (scope.options.align === "Left"){
+         tooltipClass += " tooltip-left";
+       }
+       else if (scope.options.align === "Right"){
+         tooltipClass += " tooltip-right";
+       }
 
-      // Color
-      if (scope.options.color === "Green"){
-        tooltipClass += " green";
-      }
-      else if (scope.options.color === "Blue"){
-        tooltipClass += " blue";
-      }
-      else if (scope.options.color === "Orange"){
-        tooltipClass += " orange";
-      }
-      else if (scope.options.color === "Red"){
-        tooltipClass += " red";
-      }
+       // Color
+       if (scope.options.color === "Green"){
+         tooltipClass += " green";
+       }
+       else if (scope.options.color === "Blue"){
+         tooltipClass += " blue";
+       }
+       else if (scope.options.color === "Orange"){
+         tooltipClass += " orange";
+       }
+       else if (scope.options.color === "Red"){
+         tooltipClass += " red";
+       }
 
-      // Content type
-      if (scope.options.type === "Html"){
-        tooltipClass += " content-html";
-      }
+       // Content type
+       if (scope.options.type === "Html"){
+         tooltipClass += " content-html wide extra-padding";
+       }
+ 	     else if (scope.options.type === "Download"){
+         tooltipClass += " download";
+ 	     }
+ 	     else if (scope.options.type === "Alert")	   {
+         tooltipClass += " alert"; 
+ 	     }
 
-      // Wrap element html
-      var replacementHTML = '<div class="tooltip default-style" style="opacity:1;"><span class="link">' + element.html() +
-      '<div class="' + tooltipClass + '">' + scope.data.text + '</div></span></div>';
+       // Wrap element html
+       var replacementHTML = '<span class="tooltip default-style" style="opacity:1;"><span class="link">' + element.html() +
+       '<span class="' + tooltipClass + '">' + scope.data.text + '</span></span></span>';
 
-      // Replace element's html with wrapped content
-      element.html(replacementHTML);
+       // Replace element's html with wrapped content
+       element.html(replacementHTML);
 
-    },
+     },
     controller: TooltipController,
   };
 }
