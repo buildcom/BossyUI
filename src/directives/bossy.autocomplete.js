43195a60caed7f70fc12217d179abd4319065642
Eddie Bracho <ebracho@mail.csuchico.edu>
2015-10-18 23:53:37 -0700

    AUTOCOMPLETE-163: Added missing semicolon

--- src/directives/bossy.autocomplete.js
+++ src/directives/bossy.autocomplete.js
@@ -13,10 +13,10 @@ angular.module('bossy.autocomplete', ['bossy.BKTree', 'bossy.utility'])
                 scope.maxCorrections = scope.maxCorrections || 0;
                 scope.maxSuggestions = scope.maxSuggestions || 5;
                 scope.tree = new BKTree(scope.dict, scope.maxCorrections);
-                scope.suggestions = []
+                scope.suggestions = [];
 
                 scope.updateSuggestions = function(query) {
-                    var startsWithMatches = utility.filterStartsWith(scope.dict, query, true);
+                    var startsWithMatches = utility.filterStartsWith(scope.dict, query, true),
                         correctionMatches = scope.tree.query(query);
                     if (query.length > 0) {
                         // ng-repeat doesn't work with Sets
