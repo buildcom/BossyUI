59a34aeb3db48c206cb26b6ca3f710ff7fa7bdd0
Eddie Bracho <ebracho@mail.csuchico.edu>
2015-10-18 22:42:15 -0700

    UTILITY-TESTING: Finished writing utility tests

--- test/services/bossy.utility.spec.js
+++ test/services/bossy.utility.spec.js
@@ -10,14 +10,48 @@ describe('bossyUtility tests', function() {
 
     describe('createMatrix tests', function() {
 
-        it('Matrix 0 width and height is empty array', function() {
+        it('Matrix of 0 width and height is empty array', function() {
             var mat = utility.createMatrix(0,0);
             expect(mat).toEqual([]);
+        });
+
+        it('Matrix of n width has length n', function() {
+            var mat = utility.createMatrix(3,5);
+            expect(mat.length).toEqual(3);
+        });
+
+        it('Matrix of m height contains arrays of length m', function() {
+            var i, mat = utility.createMatrix(3,5);
+            expect(mat[0].length).toEqual(5);
+            expect(mat[1].length).toEqual(5);
+            expect(mat[2].length).toEqual(5);
         });
 
     });
     
     describe('filterStartsWith tests', function() {
+        
+        it('Filtering an empty list returns []', function() {
+            var matches = utility.filterStartsWith([], 'x', false);
+            expect(matches).toEqual([]);
+        });
+
+        it('Returns [] when no words start with query', function() {
+            var matches = utility.filterStartsWith(['axx', 'bxx', 'cxx'], 'x', false);
+            expect(matches).toEqual([]);
+        });
+
+        it('Returns words that start with query', function() {
+            var words = ['car', 'star', 'can', 'cup', 'CAR'];
+            var matches = utility.filterStartsWith(words, 'ca', false);
+            expect(matches).toEqual(['car', 'can']);
+        });
+
+        it('Ignores case when caseInsensitive is true', function() {
+            var words = ['bOat', 'BoAr', 'BOnd', 'bar'];
+            var matches = utility.filterStartsWith(words, 'bo', true);
+            expect(matches).toEqual(['bOat', 'BoAr', 'BOnd']);
+        });
     });
 
 });
