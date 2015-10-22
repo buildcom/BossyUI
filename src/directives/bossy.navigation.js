5539005b245091ced85389113df7a5eab15a254b
Eddie <ebracho@mail.csuchico.edu>
2015-10-22 11:21:28 -0700

    NAVIGATION-170: Started link function, created Node class

--- src/directives/bossy.navigation.js
+++ src/directives/bossy.navigation.js
@@ -3,6 +3,21 @@ angular.module('bossy.navigation')
         return {
             restrict: 'E',
             link: function(scope, element, attrs) {
+
+                scope.Node = function(subMenuObj) {
+                    this.title = subMenuObj.title;
+                    if (angular.isString(subMenuObj.link) {
+                        this.url = subMenuObj.link;
+                    }
+                    if (angular.isObject(subMenuObj.link) {
+                        this.children = [];
+                        for (childMenuObj in subMenuObj.link) {
+                            this.children.push(new Node(childMenuObj));
+                        }
+                    }
+                };
+
+                scope.curNode = scope.rootNode = new Node(attrs.menuObj);
             }
         };
     })
