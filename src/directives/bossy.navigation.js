b54a93f93c01ba4eb15feca24092f57495642f4e
Eddie <ebracho@mail.csuchico.edu>
2015-10-23 12:06:21 -0700

    NAVIGATION-170: Created menu template

--- src/directives/bossy.navigation.js
+++ src/directives/bossy.navigation.js
@@ -1,32 +1,60 @@
-angular.module('bossy.navigation')
-    .directive('menu', function() {
+angular.module('bossy.navigation', ['bossy.data'])
+
+    .directive('menu', ['$data', function($data) {
         return {
+
             restrict: 'E',
             link: function(scope, element, attrs) {
 
-                scope.Node = function(parentNode, subMenuObj) {
-                    this.title = subMenuObj.title;
+                Node = function(parentNode, menuObj) {
+                    var i, childMenuObj;
+                    this.title = menuObj.title;
                     this.parentNode = parentNode;
-                    if (angular.isString(subMenuObj.link) {
-                        this.url = subMenuObj.link;
+                    if (angular.isString(menuObj.link)) {
+                        this.url = menuObj.link;
                     }
-                    if (angular.isObject(subMenuObj.link) {
+                    if (angular.isArray(menuObj.link)) {
                         this.children = [];
-                        for (childMenuObj in subMenuObj.link) {
-                            this.children.push(new Node(this, childMenuObj));
+                        for (i = 0; i < menuObj.link.length; i++) {
+                            this.children.push(new Node(this, menuObj.link[i]));
                         }
                     }
                 };
 
-                scope.curNode = scope.rootNode = new scope.Node(null, attrs.menuObj);
+                scope.menu = {
+                    open: false,
+                    root: null,
+                    cur: null,
+                };
 
-                scope.updateView = function() {
-                    scope.curTitle = scope.curNode.title;
-                    scope.curChildren = scope.curNode.children;
-                }
-            }
+                $data.getData(attrs.data).then(function(result) {
+                    scope.menu.cur = scope.menu.root = new Node(null, result.navigation[0]);
+                    console.log(result.navigation[0]);
+                });
+
+                scope.updateView = function(node) {
+                    scope.menu.cur = node;
+                    console.log(scope.menu.cur.children);
+                };
+
+                scope.toggleOpen = function() {
+                    scope.menu.open = !scope.menu.open;
+                    scope.menu.cur = scope.menu.root;
+                };
+
+            },
+            template: 
+               '<div>' +
+                  '<strong ng-click="toggleOpen()">{{menu.root.title}}</strong>' +
+                  '<ol ng-show="menu.open">' +
+                      '<lh ng-show="menu.cur!==menu.root" ng-click="updateView(menu.cur.parentNode)">{{menu.cur.title}}</lh>' +
+                      '<li ng-repeat="node in menu.cur.children">' +
+                          '<a ng-if="node.url" ng-attr-href="{{node.url}}">{{node.title}}</a>' +
+                          '<div ng-if="node.children" ng-click="updateView(node)">{{node.title}}</div>' +
+                      '</li>' +
+                  '</ol>' +
+              '<div>'
+
         };
-    })
+    }])
 ;
-
-
