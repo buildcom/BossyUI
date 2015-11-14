ba99c34c85d54c6968789e6ce1f71cbfb87edeca
Rogelio <rgaray-morfin@mail.csuchico.edu>
2015-11-13 16:38:42 -0800

    Implemented input, label, and select for combobox.

--- src/directives/bossy.combobox.js
+++ src/directives/bossy.combobox.js
@@ -1,71 +1,54 @@
-function testContoller($scope){
-    	$scope.elements = $scope.data.elements;
+function comboboxController($scope){
+	$scope.list = [
+	{value: 'Ball', 
+	 registered: true},
+	{value: 'Stick',
+	 registered: true},
+	{value: 'Table',
+	 registered: true},
+	{value: 'Chalk',
+	 registered: true},
+	{value: 'Cue',
+	 registered: true},
+	{value: 'Eight',
+	 registered: true},
+	{value: 'People',
+	 registered: true},
+	{value: 'Rails',
+	 registered:true
+	}];
+	 $scope.myVar = true;
+	 $scope.toggleItem = function(){
+	 };
+	 $scope.toggleView = function(){
+	 	$scope.myVar = !$scope.myVar;
+	 };
+	 $scope.getRegistered = function(){
+	 };
+	 $scope.submitItem = function(){
+	 };
+}
+function combobox(){
+	var template = '<br>' +
+	 '<head>Combobox Template</head>' +
+	  '<hr/>' +
+		'<div>' + '<br>' +  '<input type="text" ng-model="name"/>' +
+		 '<label ng-repeat="item in list | filter:{registered:false}">{{item.value}}  <input type="checkbox" ng-click="item.registered=true"/>' + 
+		 '</label>' + '<button ng-click="toggleView()" >This is an arrow</button>' +
+	   		'<p ng-hide="myVar">' +  
+	   			'<select>' +  
+	   				'<option value="">-- Select --</option>' +
+	    			'<option ng-repeat="item in list | filter:{registered:true} | filter:name" ng-click="item.registered=false">{{item.value}}</option>' + 
+	   			'</select>' + 
+	   		'</p>' +
+		'</div>';
 
-    	$scope.list = $scope.elements.slice();
-
-    	//$scope.list = [{city: 'Chico', state: 'CA'}, {city: 'LA', state: 'CA'}, {city: 'Atlanta', state: 'GA'}];
-    	
-    	$scope.selectedElements = [];
-    	
-        // Remove item from the list of selected items and place it back in the dropbox
-    	$scope.removeSelection = function(element) 
-    	{
-    		$scope.list.push(element);
-
-    		var index = $scope.selectedElements.indexOf(element);
-    		$scope.selectedElements.splice(index, 1);
-    	}
-
-        // Reset the selections and dropdown options back to default
-    	$scope.resetSelections = function()
-    	{
-    		$scope.selectedElements = [];
-    		$scope.list = $scope.elements.slice();
-    	}
-
-        // Add selected item to selection list and remove it from the dropdown box
-    	$scope.newSelection = function()
-    	{
-    		if($scope.selectedElements.indexOf($scope.selectedItem) == -1)
-    		{
-    			$scope.selectedElements.push($scope.selectedItem);
-
-    			var index = $scope.list.indexOf($scope.selectedItem);
-    			$scope.list.splice(index, 1);
-    		}
-    	}
+	return {
+		restrict: 'E',
+		template: template,
+		controller: comboboxController
+	};
 }
 
-function create(){
-	var template = 	'<br>'+
-					'<div><input type="text" data-ng-model="name"/></div>'+
-					'<select ng-model="selectedItem" ng-options="item.city group by item.state for item in list | filter:name | orderBy:state" ng-change="newSelection()">'+
-					'</select>'+
-					'<ul>'+
-						'<li data-ng-repeat="element in selectedElements">{{element.city}} <button ng-click="removeSelection(element)">x</button> </li>'+
-					'</ul>'
-					'<button ng-click="resetSelections()">reset</button>';
-					//'<div data-ng-repeat="x in elements| filter:name">{{x}}</div>';
-
-  return {
-
-    restrict: 'E',
-    
-    scope: {
-    	data: '='
-    },
-    template: template,
-    controller: testContoller,
-    
-  };
-}
-
-
-create.$inject = [];
-
-testContoller.$inject = ['$scope'];
-
-angular.module('test',[])
-		.controller('init', testContoller)
-		.directive('director', create);
-		
+angular.module('bossy.Combobox',[])
+.directive('bossyCombobox', combobox);
