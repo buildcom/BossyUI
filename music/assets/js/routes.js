05322563cf62179790eda0bcaecd6483f09c001f
Geoff Lawson <geoff.lawson@build.com>
2015-12-13 22:11:21 -0800

    DOC: standardizing docs for directives

--- music/assets/js/routes.js
+++
@@ -1,1 +0,0 @@
-var foundationRoutes = [{"name":"settings","url":"/settings","animationIn":"fadeIn","animationOut":"fadeOut","path":"templates/settings.html"},{"name":"home","url":"/","animationIn":"fadeIn","animationOut":"fadeOut","path":"templates/home.html"}]; 
