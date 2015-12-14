342059739e9a7b078c730495842049dcf7ecb28b
Geoff Lawson <geoff.lawson@build.com>
2015-12-14 08:29:42 -0800

    TREEVIEW-236: fixing some of the missing styles.

--- src/directives/bossy.treeview.js
+++ src/directives/bossy.treeview.js
@@ -1,22 +1,22 @@
 /**
  * @param {param} config
- * @param {string} [config.data={ "nodes": [{"node": { "value": "folder", "nodes": [{"node": { "value": "folder2", "nodes": [ { "item": { "value": "file3"}}]}} , { "item": { "value": "file4"}}]}}]}] - Sample tree data.
+ * @param {string} [config.data={ "nodes": [{"node": { "value": "folder", "nodes": [{"node": { "value": "folder2", "nodes": [ { "item": { "value": "file3.png", "type":"png"}}]}} , { "item": { "value": "file4.jpg", "type":"jpg"}}]}}]}] - Sample tree data.
  */
 function Treeview ($compile) {
 
 	var template = '' +
-		'<ul ng-if="contents.nodes">' +
+		'<ul class="bossy-treeview" ng-if="contents.nodes">' +
 		'<li ng-repeat="node in contents.nodes" >' +
 		'<span ng-if="node.node">' +
-		'<div class="nodeIcon collapsed" ng-click="nodeClicked($event, $index)" ' +
-		'ng-class="showChildren($index) ? \'expanded\' : \'collapsed\'"> ' +
+		'<div class="bossy-treeview-nodeIcon bossy-treeview-collapsed" ng-click="nodeClicked($event, $index)" ' +
+		'ng-class="showChildren($index) ? \'bossy-treeview-expanded\' : \'collapsed\'"> ' +
 		'{{node.node.value}}' +
 		'</div>' +
 		'<bossy-treeview ng-show="showChildren($index)" data="node.node"></bossy-treeview>' +
 		'</span>' +
 		'<span ng-if="node.item">' +
-		'<div class="fileIcon"ng-click="itemClicked($event, $index)">' +
-		'{{node.item.value}}' + '<div class="content-type- {{node.item.type}}">' +
+		'<div class="bossy-treeview-fileIcon"ng-click="itemClicked($event, $index)">' +
+		'{{node.item.value}}' + '<div class="bossy-treeview-content-type-{{node.item.type}}">' +
 		'<span ng-if="node.item.type">' + '{{node.item.type}}' +
 		'</span>' + '</div>' +
 		'</div>' +
@@ -72,10 +72,10 @@ function TreeviewController ($scope, $rootScope) {
 
 	$scope.changeSelection = function ($event) {
 		if (!angular.isUndefined($rootScope.curSelectedTV)) {
-			$rootScope.curSelectedTV.toggleClass('selected');
+			$rootScope.curSelectedTV.toggleClass('bossy-treeview-selected');
 		}
 		$rootScope.curSelectedTV = angular.element($event.target);
-		$rootScope.curSelectedTV.toggleClass('selected');
+		$rootScope.curSelectedTV.toggleClass('bossy-treeview-selected');
 	};
 }
 
