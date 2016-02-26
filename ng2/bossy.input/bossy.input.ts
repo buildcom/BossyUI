4619edbb6b082c9077afb3033e6a07f65487535e
lcheung <lcheung90@gmail.com>
2016-02-25 16:13:07 -0800

    [Feature] Define selector and template for each input component type

--- ng2/bossy.input/bossy.input.ts
+++ ng2/bossy.input/bossy.input.ts
@@ -1,6 +1,23 @@
 import {Component} from 'angular2/core';
 
 @Component({
+	selector: 'bossy-input',
+	templateUrl: 'bossy-input-default.html'
+})
+
+@Component({
+	selector: 'bossy-input-prefix',
+	templateUrl: 'bossy-input-prefix.html'
+})
+
+@Component({
+	selector: 'bossy-input-postfix',
+	templateUrl: 'bossy-input-postfix.html'
+})
+
+@Component({
+	selector: 'bossy-input-counter',
+	templateUrl: 'bossy-input-counter.html'
 })
 
 export class BossyInput{}
