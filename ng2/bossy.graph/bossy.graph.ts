82e57fc628fe673641470a85a4b1d691db73b927
lcheung <lcheung90@gmail.com>
2016-03-15 21:16:54 -0700

    [Feature] Define components for graph directive

---
+++ ng2/bossy.graph/bossy.graph.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+  selector: 'bossy-graph-line',
+  inputs['config'],
+  templateUrl: 'bossy-graph-line.html'
+})
+
+@Component({
+  selector: 'bossy-graph-dot',
+  inputs['config'],
+  templateUrl: 'bossy-graph-dot.html'
+})
+
+@Component({
+  selector: 'bossy-graph-bar',
+  inputs['config'],
+  templateUrl: 'bossy-graph-bar.html'
+})
