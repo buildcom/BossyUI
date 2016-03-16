080388c036603ad2cc31e6ec0c2ff10ec3315cd3
lcheung <lcheung90@gmail.com>
2016-03-16 15:03:02 -0700

    [Feature] Define BossyGraph class
    Implement buildBarSVG function

--- ng2/bossy.graph/bossy.graph.ts
+++ ng2/bossy.graph/bossy.graph.ts
@@ -17,3 +17,37 @@ import {Component, OnInit} from 'angular2/core';
   inputs['config'],
   templateUrl: 'bossy-graph-bar.html'
 })
+
+export class BossyGraph implements OnInit{
+  public config: BossyGraphConfig;
+
+  buildBarSVG(width, height, data){
+  var graphMarkup = []
+  graphMarkup.push('<svg style="width:',
+                    width,
+                    'px; height:',
+                    height,
+                    'px;">');
+
+  var dataSetLength = data.length;
+  data.forEach(function(bar, index) {
+    var x = index * (width / dataSetLength);
+    var y = height - bar;
+    var w = width / dataSetLength;
+
+    graphMarkup.push('<rect x="',
+                      x,
+                      '" y="',
+                      y,
+                      '" width="',
+                      w,
+                      '" height="',
+                      bar,
+                      '" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)"></rect>');
+  });
+
+  graphMarkup.push('</svg>');
+
+  return graphMarkup.join("");
+  }
+}
