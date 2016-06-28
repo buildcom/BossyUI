466f662e64c09fcd5e645d649c8d43f16aeb4fd6
Taber Fitzgerald <taber.fitzgerald@build.com>
2016-06-28 08:17:39 -0700

    TestExample - Re-does tests for type script and adds a couple small examples of writing a test.

---
+++ src/components/calendar.spec.ts
@@ -0,0 +1,12 @@
+import {BossyCalendar} from '../components/calendar';
+
+describe('the tooltip component', () => {
+	it('should have a function setActive that is active when persisting', () => {
+		var calendar = new BossyCalendar();
+		var year = new Date().getFullYear();
+		var month = 1;
+		var date = new Date(year, month, 1);
+		calendar.selectMonth(month);
+		expect(calendar.display).toEqual(date);
+	})
+});
---
+++ src/components/tooltip.spec.ts
@@ -0,0 +1,8 @@
+import {BossyTooltip} from '../components/tooltip';
+
+describe('the tooltip component', () => {
+    it('should have a function setActive that is active when persisting', () => {
+        var tooltip = new BossyTooltip();
+        expect(tooltip.setActive(true)).toEqual('active');
+    })
+});
