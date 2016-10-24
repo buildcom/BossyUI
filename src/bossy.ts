9734bb5c7a552da71e107086ca139b5c97381e09
Geoff Lawson <geoff.lawson@build.com>
2016-10-24 13:51:14 -0700

    327: fixes to typings.

--- src/bossy.ts
+++ src/bossy.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BossyCalendar, BossyCalendarConfig }  from './components/calendar';
+import { BossyCalendar }  from './components/calendar';
 
 @NgModule({
     imports: [
