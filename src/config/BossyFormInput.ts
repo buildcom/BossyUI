61e8b76857215fdc49c16b4eca6d7da2fca9c64b
Kevin Lien <klien1@mail.csuchico.edu>
2016-11-13 21:15:32 -0800

    added more config options

--- src/config/BossyFormInput.ts
+++ src/config/BossyFormInput.ts
@@ -1,7 +1,18 @@
 export class BossyFormInputConfig{
     controlType: string;
+    id: string;
+    required: boolean;
+    xPosition: number;
+    yPosition: number;
+    label: string;
+
 
     constructor(controlType: string) {
         this.controlType = controlType;
+        this.id = '';
+        this.required = false;
+        this.xPosition = 0;
+        this.yPosition = 0;
+        this.label = '';
     }
 }
