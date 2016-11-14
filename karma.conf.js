ad713f4bd647dcbf3f35ef3f74b1096af8725f98
JasonGroven <JasonGroven@users.noreply.github.com>
2016-11-14 12:08:40 -0800

    Removes autowatch from systemjs.config

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
 			{ pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
 			{ pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
 
-			{ pattern: 'site/app/systemjs.config.js', included: false, watched: true },
+			{ pattern: 'site/app/systemjs.config.js', included: false, watched: false },
 			'karma-test-shim.js',
 
 			// transpiled application & spec code paths loaded via module imports
