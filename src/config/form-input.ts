b6666c26ea48ba5ffa4e6720a76d2c0f910236f0
Garrett Swann <Garrettswann3049@yahoo.com>
2016-12-05 00:12:23 -0800

    Added textarea type to text-input, as well as 'lines' to the config -- default is 1'

--- src/config/form-input.ts
+++ src/config/form-input.ts
@@ -3,11 +3,13 @@ export class BossyFormInputConfig{
     id: string;
     required: boolean;
     label: string;
+    lines: number;
 
     constructor(type: string) {
         this.type = type;
         this.id = '';
         this.required = false;
         this.label = '';        
+        this.lines = 1;
     }
 }
