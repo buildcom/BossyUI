8ef53a0dac228983e637b79c78e1f940caadad66
JasonGroven <jgroven@mail.csuchico.edu>
2016-12-06 15:11:25 -0800

    resolves #397.  Adds kjhtml to Karma, fixes a couple tests, and fixes form-input.ts

--- test/bossyconfig.spec.ts
+++ test/bossyconfig.spec.ts
@@ -1,10 +1,15 @@
-import { BossyFormConfig } from '../src/components/BossyForm';
+import { BossyFormConfig } from '../src/config/form';
+import { BossyFormInputConfig} from '../src/config/form-input';
 
-
+let cfgI: BossyFormInputConfig;
 let cfg: BossyFormConfig;
+let elem: Array<BossyFormInputConfig>;
 describe('The Config Component', () => {
 	beforeEach(() => {
-		cfg = new BossyFormConfig();
+		cfgI = new BossyFormInputConfig("text", "text", "text", "text", false, "text");
+		elem = new Array<BossyFormInputConfig>;
+		elem[0] = cfgI;
+		cfg = new BossyFormConfig(elem);
 	});
 	it('Should instantiate.', () =>{
 		expect(cfg).toBeTruthy();
