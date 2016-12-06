14c156d2ceed4bb0d14b225c6eeddfa3525f020a
JasonGroven <jgroven@mail.csuchico.edu>
2016-12-06 15:13:26 -0800

    set autowatch back to true, since singlerun is false now (karma.conf)

--- karma.conf.js
+++ karma.conf.js
@@ -85,7 +85,7 @@ module.exports = function(config) {
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
-		autoWatch: false,
+		autoWatch: true,
 		browsers: ['Chrome'],
 		singleRun: false,//Allows inspection of tests in DEBUG link
 	})
