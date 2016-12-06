2f18c5f181cf2dc3083e35a716b14e1b6fc7b141
Dustin Jackson <jacksondk82@gmail.com>
2016-12-05 17:00:49 -0800

    Actually fixed config/form-input.ts

--- src/config/form-input.ts
+++ src/config/form-input.ts
@@ -13,12 +13,10 @@ export class BossyFormInputConfig{
     constructor(name: string, type: string, cssClass: string, id: string, required: boolean, label: string) {
         this.name = name;
         this.type = type;
-<<<<<<< HEAD
         this.cssClass = cssClass;
         this.id = id;
         this.required = required;
         this.label = label;
-=======
         this.id = '';
         this.required = false;
         this.label = '';
@@ -26,6 +24,5 @@ export class BossyFormInputConfig{
         this.cols = 75;
         this.name = '';
         this.placeholder = '';
->>>>>>> 9fc819cd72c2aabdddb29dcf6505cb7ce3b7a9c1
     }
 }
