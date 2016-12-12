43c530d5eec08fa9e3eaca96b5dd0a599f3011ce
Kevin Lien <klien8589@gmail.com>
2016-12-11 21:16:38 -0800

    408: added label-config to form-input-config

--- src/config/form-input.ts
+++ src/config/form-input.ts
@@ -1,14 +1,15 @@
 import {FormGroup} from '@angular/forms';
+import {BossyFormLabelConfig} from './form-label';
 
 export class BossyFormInputConfig {
 	constructor(
 		public name: string,
 		public type: string,
 		public value?: string,
+		public label?: BossyFormLabelConfig,
 		public id?: string,
 		public cssClass?: string,
 		public required?: string,
-		public label?: string,
 		public rows?: number,
 		public cols?: number,
 		public placeholder?: string,
