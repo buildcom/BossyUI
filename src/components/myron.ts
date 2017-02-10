f8dc9f2efa7bba0e7dcf59c2963415dd354db138
Myron Kant <Lobstersalad@gmail.com>
2017-02-09 21:44:42 -0800

    Adding my component for 419

---
+++ src/components/myron.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'Myron-Kant',
+	template:
+		`
+		<h3>{{name}}</h3>
+		`
+});
+
+export class ExampleComponent {
+	name = 'Myron';
+}
