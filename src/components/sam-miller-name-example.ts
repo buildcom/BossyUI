32daf012e14280285135c91c0456bd7a2ecc665c
Sam Miller <samuelmiller140@hotmail.com>
2017-02-14 11:16:50 -0800

    updates sam-miller-name-example.ts

--- src/components/sam-miller-name-example.ts
+++ src/components/sam-miller-name-example.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sam-miller',
-  template: `<h1>Sam Miller</h1>`
+  template: `<h3>{{name}}</h3>`
 })
-export class SamMillerComponent { isLoggedIn: boolean = false; }
+export class SamMillerComponent { name = 'Sam Miller'; }
