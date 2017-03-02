eb026018fa4a9226c9155e4bc73300f695b66695
lcramer3 <lcramer3@mail.csuchico.edu>
2017-03-01 19:06:22 -0800

    Updated bossyUI to BS 4 CSS styling

--- src/config/form-label.ts
+++ src/config/form-label.ts
@@ -3,5 +3,8 @@ export class BossyFormLabelConfig {
 		public text?: string,
 		public inline?: boolean,
 		public cssClass?: string,
+		public stateDanger?: boolean,
+		public stateWarning?: boolean,
+		public stateSuccess?: boolean
 	) {}
 }
