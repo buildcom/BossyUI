9b049da103e40cc255324c83f0a5a20e8d9201c4
Myron Kant <Lobstersalad@gmail.com>
2017-04-01 12:36:38 -0700

    Fixed templateUrl, deleted myron.ts

--- src/components/radio.ts
+++ src/components/radio.ts
@@ -2,20 +2,12 @@ import {Component, Input, SimpleChanges} from '@angular/core';
 import {BossyRadioConfig} from '../config/radio';
 import {BossyFormConfig} from '../config/form';
 
+declare const module: any;
 
 @Component({
+	moduleId: module.id,
 	selector: 'bossy-radio',
-  templateUrl: '../templates/radio.html'
-/*
-	template: `
-	<div *ngFor="let element of items; let i = index" [ngClass]="{'form-check': true, ' form-check-inline': isInline}">
-	  <label class="form-check-label">
-	    <input class="form-check-input" type="radio" name="radioOptions" id="radioElement{{i}}" value="option{{i}}">  {{element}}
-	  </label>
-	</div>
-	`
-	browser/something things that '..' is part of the url, rather than following relative path
-*/
+	templateUrl: '../templates/radio.html'
 })
 export class BossyRadio {
 	@Input() config: BossyRadioConfig;
