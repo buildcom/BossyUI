e3802373aae2096517decca428e9a4b332026ddd
Myron Kant <Lobstersalad@gmail.com>
2017-04-04 16:55:48 -0700

    Another attempt at unit testing

--- test/radio.spec.ts
+++ test/radio.spec.ts
@@ -1,20 +1,23 @@
+/*
+
 // Following example from other BossyUI tests
 
 import {BossyRadio} from '../src/components/radio';
 
 let radio: BossyRadio;
 
-describe('Radio component', () => {
+fdescribe('Radio component', () => {
 	beforeEach(() => {
 		radio = new BossyRadio();
+		radio.config = {items: []}
 		radio.ngOnInit();
 	});
-	it('banana check', () => {
-		expect("bananas").toEqual("bananas");
+	it('sanity check', () => {
+		expect(true).toEqual(false);
 	});
 });
 
-/*
+*/
 
 // Following example from angular.io testing docs
 
@@ -22,27 +25,35 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {DebugElement} from '@angular/core';
 import {BossyRadio} from '../src/components/radio';
+import {BossyRadioConfig} from '../src/config/radio';
 
 fdescribe('Radio Component', () => {
 	let radio: BossyRadio;
+	let bossyRadioConfig: BossyRadioConfig;
+	let bossyRadioConfigStub: BossyRadioConfig;
 	let fixture: ComponentFixture<BossyRadio>;
 	let de: DebugElement;
 	let el: HTMLElement;
 	beforeEach(async(() => {
-  	TestBed.configureTestingModule({
-    declarations: [BossyRadio],
-  	})
-  	.compileComponents();
+		bossyRadioConfigStub = {
+			items: ['bananas'],
+			isInline: false
+		};
+		TestBed.configureTestingModule({
+			declarations: [BossyRadio],
+			providers: [{provide: BossyRadioConfig, useValue: bossyRadioConfigStub}]
+		})
+		.compileComponents();
 	}));
+
 	beforeEach(() => {
 		fixture = TestBed.createComponent(BossyRadio);
 		radio = fixture.componentInstance;
-		de = fixture.debugElement.query(By.css('div'));
+		bossyRadioConfig = TestBed.get(BossyRadioConfig);
+		de = fixture.debugElement.query(By.css('label'));
 		el = de.nativeElement;
 	});
 	it('banana check', () => {
 		expect("bananas").toEqual("bananas");
 	});
 });
-
-*/
