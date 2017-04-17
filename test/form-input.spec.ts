684cfedc3ff5e481bfd78397856c9785ff7ba94c
samcoder140 <samuelmiller140@hotmail.com>
2017-04-17 14:17:34 -0700

    adds draft of unit test

---
+++ test/form-input.spec.ts
@@ -0,0 +1,11 @@
+import {BossyFormInput} from '../src/components/form-input';
+
+let formIn: BossyFormInput;
+
+describe('the form input component', () => {
+	it('should display an empty input field', () => {
+
+		expect(form-input.display).toEqual(date);
+		expect(true).toBeFalsy();
+	});
+});
