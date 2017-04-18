82adfa375d1cff2722df30ebb30123de4c0d64c9
samcoder140 <samuelmiller140@hotmail.com>
2017-04-17 18:23:37 -0700

    changes 'fit' back to 'it' so all tests are run

--- test/form-input.spec.ts
+++ test/form-input.spec.ts
@@ -4,19 +4,19 @@ let inp: BossyFormInput;
 let formInput = new BossyFormInput();
 
 describe('the form input component', () => {
-	fit('should set default hasSuccess to false', () => {
+	it('should set default hasSuccess to false', () => {
 		expect(formInput.hasSuccess).toEqual(false);
 	});
 });
 
 describe('the form input component', () => {
-	fit('should set default hasWarning to false', () => {
+	it('should set default hasWarning to false', () => {
 		expect(formInput.hasWarning).toEqual(false);
 	});
 });
 
 describe('the form input component', () => {
-	fit('should set default hasDanger to false', () => {
+	it('should set default hasDanger to false', () => {
 		expect(formInput.hasDanger).toEqual(false);
 	});
 });
