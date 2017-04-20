7de208d5d93427781ede2e7a671a606548fd8ec2
samcoder140 <samuelmiller140@hotmail.com>
2017-04-19 20:03:52 -0700

    WIP - form-input unit tests

--- test/form-input.spec.ts
+++ test/form-input.spec.ts
@@ -1,22 +1,66 @@
+import { async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
 import {BossyFormInput} from '../src/components/form-input';
+import {BossyFormInputConfig} from '../src/config/form-input';
 
 let inp: BossyFormInput;
-let formInput = new BossyFormInput();
+let fixture: ComponentFixture<BossyFormInput>;
+let de:      DebugElement;
+let el:      HTMLElement;
+let formInpConfig: BossyFormInputConfig;
+
+
+describe('Unit tests for BossyFormInput Component: ', () => {
+	describe('Testbed to check textContent of <label>s', () => {
+		beforeEach(async(() => {
+			TestBed.configureTestingModule({
+				declarations: [BossyFormInput],
+			})
+			.compileComponents(); // compile template and css
+	}));
+
+
+		beforeEach(() => {
+			fixture = TestBed.createComponent(BossyFormInput);
+			inp = fixture.componentInstance;
+			formInpConfig = new BossyFormInputConfig('emailInput');
+			inp.config = formInpConfig;
+			inp.ngOnInit();
+			fixture.detectChanges();
+		});
+
+		it('first item of 3', () => {
+			de = fixture.debugElement.query(By.css('#Random'));
+			el = de.nativeElement;
+			expect(el.textContent).toContain('Superman');
+		});
+
+	});
+
+
+
+
+
 
 describe('the form input component', () => {
 	it('should set default hasSuccess to false', () => {
-		expect(formInput.hasSuccess).toEqual(false);
+		expect(inp.hasSuccess).toEqual(false);
 	});
 });
 
 describe('the form input component', () => {
 	it('should set default hasWarning to false', () => {
-		expect(formInput.hasWarning).toEqual(false);
+		expect(inp.hasWarning).toEqual(false);
 	});
 });
 
 describe('the form input component', () => {
 	it('should set default hasDanger to false', () => {
-		expect(formInput.hasDanger).toEqual(false);
+		expect(inp.hasDanger).toEqual(false);
 	});
+});
+
 });
