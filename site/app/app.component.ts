f1df42b772533057d72565ef45935723b2b8d65c
Myron Kant <Lobstersalad@gmail.com>
2017-05-01 13:02:29 -0700

    Small changes

--- site/app/app.component.ts
+++ site/app/app.component.ts
@@ -98,6 +98,7 @@ export class AppComponent {
 				new BossyFormInputConfig(formInput1),
 				new BossyFormInputConfig(formInput2),
 				new BossyFormInputConfig(formInput3),
+				new BossyFormInputConfig(forminput4),
 				new BossyFormInputConfig(formInput5),
 				new BossyFormInputConfig(formInput6)
 			],
