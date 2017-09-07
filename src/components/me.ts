5cbeb4bf845b18624d306750aadf849449f675bb
Taber Fitzgerald <taber.fitzgerald@build.com>
2017-09-07 09:04:13 -0700

    student-example - Student example of build a simple component in typescript

---
+++ src/components/me.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+declare const module: any;
+
+@Component({
+	moduleId: module.id,
+	selector: 'bossy-me',
+	templateUrl: '../templates/me.html',
+	styleUrls: ['../styles/me.css']
+})
+
+export class Me {
+	showHobbies = false;
+	constructor() {
+
+	}
+	click() {
+		this.showHobbies = !this.showHobbies;
+	}
+}
