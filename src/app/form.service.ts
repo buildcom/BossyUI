14701c9ea18840b6e2df03803e73824c06f6f32c
oliverrayreed <oliverrayreed@gmail.com>
2017-10-04 10:47:24 -0700

    added formservice

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -1,29 +1,27 @@
 import { Injectable } from '@angular/core';
+import {BossyCalendarComponent} from '../bossy-ui/components/calendar/calendar'
+import {BossyDropdownComponent} from '../bossy-ui/components/dropdown/dropdown'
+import {BossyDropdownMenuItemComponent} from '../bossy-ui/components/dropdown-menu/dropdown-menu'
+import {BossyFormComponent} from '../bossy-ui/components/form/form';
+import {BossyFormInputComponent} from '../bossy-ui/components/form-input/form-input';
+import {BossyFormInputErrorComponent} from '../bossy-ui/components/form-input-error/form-input-error'
+import {BossyFormLabelComponent} from '../bossy-ui/components/form-label/form-label';
+import {BossyFormRadioComponent} from '../bossy-ui/components/form-radio/form-radio';
+import {BossyFormSelectMenuComponent} from '../bossy-ui/components/form-selectmenu/form-selectmenu';
+import {BossyFormTextareaComponent} from '../bossy-ui/components/form-textarea/form-textarea';
+import {BossySliderComponent} from '../bossy-ui/components/slider/slider';
 
 @Injectable()
-export class BossyDropdownMenu{
-    type = 'button';
-    name = 'missingName';
-    href = '#';
-    isDisabled = false;
+export class FormService{
+    calendar : BossyCalendarComponent;
+    dropdown : BossyDropdownComponent;
+    dropdownmenu: BossyDropdownMenuItemComponent;
+    formcomponent: BossyFormComponent;
+    forminput : BossyFormInputComponent;
+    forminpurerror: BossyFormInputErrorComponent;
+    formlabel: BossyFormLabelComponent;
+    formradio: BossyFormRadioComponent;
+    formselectmenu: BossyFormSelectMenuComponent;
+    formtextarea: BossyFormTextareaComponent;
+    slider: BossySliderComponent;
 }
-
-/*
-.service('BossyDropdownMenu', function(){
-    //this.selector = 'bossy-dropdown-menu';
-    //this.templateURL = './dropdown-menu.html';
-    this.type = 'button';
-    this.name = 'missingName';
-    this.href = '#';
-    this.isDisabled = false;
-});
-
-.service('BossyDropDownComponent'), function(){
-    this.type = 'Button';
-    this.isSplit = false;
-    this.isDropup = false;
-    this.isRightAligned = false;
-    this.variant = 'secondary';
-   // this.size: string = undefined;
-}
-*/
