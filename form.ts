bcda020e793fec5252ca81d46d379eb511de3aeb
Mriewer <31418852+Mriewer@users.noreply.github.com>
2017-10-09 12:05:11 -0700

    Add files via upload

--- form.ts
+++ form.ts
@@ -1,52 +1,16 @@
-import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {BossyFormConfig} from '../../config/form';
+import {BossyFormInputConfig} from './form-input';
 
-@Component({
-  selector: 'bossy-form',
-  templateUrl: './form.html',
-  styleUrls: ['./form.css'],
-})
-export class BossyFormComponent implements OnInit {
-  @Input() config: BossyFormConfig;
-  bossyForm: FormGroup;
-  isFormInlinedFromConfig = false;
-  isFormLabeledFromConfig = false;
-  isFormDefaultGroupFromConfig = false;
-  isFormGridFromConfig = false;
-  isFormCompactGridFromConfig = false;
-  isFormHorizontalFromConfig = false;
-  isFormManualSizedFromConfig = false;
-  isFormAutoSizedFromConfig = false;
-  isFormDisabledFromConfig = false;
-  isFormValidCheckedFromConfig = false;
-
-  constructor(private formBuilder: FormBuilder) {
-  }
-
-  ngOnInit() {
-    const elements: any = {};
-
-    this.isFormInlinedFromConfig = this.config.isFormInlined;
-    this.isFormLabeledFromConfig = this.config.isFormLabeled;
-    this.isFormDefaultGroupFromConfig = this.config.isFormDefaultGroup;
-    this.isFormGridFromConfig = this.config.isFormGrid;
-    this.isFormCompactGridFromConfig = this.config.isFormCompactGrid;
-    this.isFormHorizontalFromConfig = this.config.isFormHorizontal;
-    this.isFormManualSizedFromConfig = this.config.isFormManualSized;
-    this.isFormAutoSizedFromConfig = this.config.isFormAutoSized;
-    this.isFormDisabledFromConfig = this.config.isFormDisabled;
-    this.isFormValidCheckedFromConfig = this.config.isFormValidChecked;
-
-    this.config.elements.forEach((element) => {
-      const {name, value} = element;
-      elements[name] = value;
-    });
-
-    this.bossyForm = this.formBuilder.group(elements);
-  }
-
-  onSubmit() {
-    // TODO: return form data
+export class BossyFormConfig {
+  constructor(public elements: Array<BossyFormInputConfig>,
+              public isFormInlined?: boolean,
+              public isFormLabeled?: boolean,
+              public isFormDefaultGroup?: boolean,
+              public isFormGrid?: boolean,
+              public isFormCompactGrid?: boolean,
+              public isFormHorizontal?: boolean,
+              public isFormManualSized?: boolean,
+              public isFormAutoSized?: boolean,
+              public isFormDisabled?: boolean,
+              public isFormValidChecked?: boolean) {
   }
 }
