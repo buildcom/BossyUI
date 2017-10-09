e487dcae183b9f292d9597ad0cf7eaecdc9cd168
Mriewer <31418852+Mriewer@users.noreply.github.com>
2017-10-09 12:00:23 -0700

    work in progress issue #527
    Form config now aligns more closely to Bootstrap options, but the service for data connection is still a work in progress

---
+++ form.service.ts
@@ -0,0 +1,7 @@
+import {Injectable} from '@angular/core';
+import {BossyFormConfig} from '../bossy-ui/config/form';
+
+@Injectable()
+export class FormService{
+    getFormConfig(): void{} //stub
+}
---
+++ form.ts
@@ -0,0 +1,52 @@
+import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {BossyFormConfig} from '../../config/form';
+
+@Component({
+  selector: 'bossy-form',
+  templateUrl: './form.html',
+  styleUrls: ['./form.css'],
+})
+export class BossyFormComponent implements OnInit {
+  @Input() config: BossyFormConfig;
+  bossyForm: FormGroup;
+  isFormInlinedFromConfig = false;
+  isFormLabeledFromConfig = false;
+  isFormDefaultGroupFromConfig = false;
+  isFormGridFromConfig = false;
+  isFormCompactGridFromConfig = false;
+  isFormHorizontalFromConfig = false;
+  isFormManualSizedFromConfig = false;
+  isFormAutoSizedFromConfig = false;
+  isFormDisabledFromConfig = false;
+  isFormValidCheckedFromConfig = false;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    const elements: any = {};
+
+    this.isFormInlinedFromConfig = this.config.isFormInlined;
+    this.isFormLabeledFromConfig = this.config.isFormLabeled;
+    this.isFormDefaultGroupFromConfig = this.config.isFormDefaultGroup;
+    this.isFormGridFromConfig = this.config.isFormGrid;
+    this.isFormCompactGridFromConfig = this.config.isFormCompactGrid;
+    this.isFormHorizontalFromConfig = this.config.isFormHorizontal;
+    this.isFormManualSizedFromConfig = this.config.isFormManualSized;
+    this.isFormAutoSizedFromConfig = this.config.isFormAutoSized;
+    this.isFormDisabledFromConfig = this.config.isFormDisabled;
+    this.isFormValidCheckedFromConfig = this.config.isFormValidChecked;
+
+    this.config.elements.forEach((element) => {
+      const {name, value} = element;
+      elements[name] = value;
+    });
+
+    this.bossyForm = this.formBuilder.group(elements);
+  }
+
+  onSubmit() {
+    // TODO: return form data
+  }
+}
