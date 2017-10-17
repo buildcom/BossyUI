eaeb95e557d971abef3de42ad31411a539face87
oliverrayreed <oliverrayreed@gmail.com>
2017-10-16 22:58:22 -0700

    Resolved whitespace error and added a few declarations

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class FormService{
-    TYPE = 'Button';
+export class FormConfigService {
+    TYPE: string = undefined;
     ISSPLIT = false;
     ISDROPUP = false;
     ISRIGHTALIGNED = false;
     VARIANT = 'Secondary';
+    SIZE: string = undefined;
     NAME = 'MissingName';
     HREF = '#';
     HASVALIDATION = true;
+    DANGER = 'Danger'
+    WARNING = 'Warning';
+    ERROR = 'Error';
+    ISINLINE = false;
 }
