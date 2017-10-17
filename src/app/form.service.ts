c6789f12c67fd32aaa036e804a9cb1e4fb044734
oliverrayreed <oliverrayreed@gmail.com>
2017-10-16 23:01:32 -0700

    Fixed missing semicolon

--- src/app/form.service.ts
+++ src/app/form.service.ts
@@ -11,7 +11,7 @@ export class FormConfigService {
     NAME = 'MissingName';
     HREF = '#';
     HASVALIDATION = true;
-    DANGER = 'Danger'
+    DANGER = 'Danger';
     WARNING = 'Warning';
     ERROR = 'Error';
     ISINLINE = false;
