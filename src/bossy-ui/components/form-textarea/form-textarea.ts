4d58d862402ffbe93634b6b425a4cb534580b04c
ClassicSours <aaron.c.shaffer@gmail.com>
2017-11-13 13:07:06 -0800

    578 updated validators

--- src/bossy-ui/components/form-textarea/form-textarea.ts
+++ src/bossy-ui/components/form-textarea/form-textarea.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators, NgModel} from '@angular/forms';
 import {BossyFormTextareaConfig} from '../../config/bossy-form-textarea';
 
 @Component({
@@ -9,7 +9,9 @@ import {BossyFormTextareaConfig} from '../../config/bossy-form-textarea';
 })
 export class BossyFormTextareaComponent implements OnInit {
   @Input() config: BossyFormTextareaConfig;
-  hasValidation = 'none';
+  // hasValidation = 'none';
+  value = '';
+  form = new FormGroup({})
 
   constructor() {
   }
@@ -17,9 +19,34 @@ export class BossyFormTextareaComponent implements OnInit {
   ngOnInit() {
     const {name, value, formGroup} = this.config;
 
-    if (formGroup) {
-      formGroup.addControl(name, new FormControl(value));
-    }
+    // value = element
+    // validators = list of validators for the element
+    // this.form.controls.
+      // this.form.addControl(value, new FormControl(value, {
+        // validators: Validators.required
+      // Validators.require()
+    // }));
+    Validators.minLength(1);
+    const type = 'minlength';
+    const type_value = 1;
+    Validators[type](type_value);
+    Validators['required'](null);
+    // {minlength: {1}}
+    // {email : {{required,null}, {minlength,1}, {type,string}}}
+
+    // config.
+    //  elements.
+    //    type: 'email;
+    //      validators: [
+    //       {type: required},
+    //       {type:'minlength',value: }
+    //      ]
+  }
+
+  onChange(value:string) {
+      this.value = value;
+      console.log("Value is: %s", this.value)
+      this.form.patchValue({value : this.value})
   }
 
   output() {
