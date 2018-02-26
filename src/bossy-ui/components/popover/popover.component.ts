0364d03f9c9e3674488aecff94b0e96145cbfa9e
ZachW628 <ztron628@gmail.com>
2018-02-26 09:24:07 -0800

    Fixed the component so it can dynamically place popover window.
    Signed-off-by: ZachW628 <ztron628@gmail.com>

--- src/bossy-ui/components/popover/popover.component.ts
+++ src/bossy-ui/components/popover/popover.component.ts
@@ -10,12 +10,25 @@ import {BossyPopoverConfig} from './popover.config';
 export class BossyPopoverComponent implements OnInit {
   @Input() config: BossyPopoverConfig;
   hide = false;
-  ngOnInit() {  }
+  Top = "0px";
+  Left = "0px";
+  ngOnInit() {
+    this.config.popoverLeft = 0;
+    this.Top = "0px";
+    this.Left = "0px";
+    }
 
   ShowPopoverOnClick(event: any) {
-
+    {/*this.config.popoverTop = event.target.offsetTop;
+    this.config.popoverLeft = event.target.offsetLeft + event.target.offsetWidth;*/}
+    this.Top = (event.target.offsetTop - 34) + "px";
+    this.Left = event.target.offsetLeft + event.target.offsetWidth + "px";
     this.hide = !this.hide;
-    console.log('help');
+    console.log(event.target.offsetTop);
+    console.log(event.target.offsetHeight);
+    console.log(event.target.parentElement.offsetTop);
+    console.log(event.target.offsetWidth);
+
   }
 
 }
