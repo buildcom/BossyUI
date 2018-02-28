f04378e19bd9f891a0cc49ca77d9860053828da2
ZachW628 <ztron628@gmail.com>
2018-02-28 09:55:36 -0800

    Fixed Popover to disable when click outside Finished
    Signed-off-by: ZachW628 <ztron628@gmail.com>

--- src/bossy-ui/components/popover/popover.component.ts
+++ src/bossy-ui/components/popover/popover.component.ts
@@ -1,28 +1,40 @@
-import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnInit, SimpleChanges, ElementRef} from '@angular/core';
 import {BossyPopoverConfig} from './popover.config';
+import { HostListener } from '@angular/core/src/metadata/directives';
 
 
 @Component({
   selector: 'bossy-popover',
   templateUrl: './popover.component.html',
-  styleUrls: ['./popover.component.css']
+  styleUrls: ['./popover.component.css'],
+  host: {'(document:click)': 'onClick($event)'}
 })
+
 export class BossyPopoverComponent implements OnInit {
   @Input() config: BossyPopoverConfig;
+
+  constructor(private _eref: ElementRef) {}
   hide = false;
   Top = "0px";
   Left = "0px";
   ngOnInit() {
-    this.config.popoverLeft = 0;
     this.Top = "0px";
     this.Left = "0px";
     this.hide = false;
+  }
+
+  onClick(event: any) {
+    if (this.hide) {
+      if (!this._eref.nativeElement.contains(event.target))
+        this.hide = false;
     }
+  }
 
   ShowPopoverOnClick(event: any) {
     this.Top = (event.target.offsetTop - 34) + "px";
-    this.Left = event.target.offsetLeft + event.targest.offsetWidth + "px";
+    this.Left = event.target.offsetLeft + event.target.offsetWidth + "px";
     this.hide = !this.hide;
   }
+
 
 }
