d6834fdeda0314ab55047d7ff8b2ff9ee2ac1af2
Unknown <mescutia@mail.csuchico.edu>
2018-02-28 10:44:54 -0800

    645: Simplified dropdown config files for refresh.

--- src/bossy-ui/components/dropdown/dropdown.config.ts
+++ src/bossy-ui/components/dropdown/dropdown.config.ts
@@ -2,12 +2,6 @@ import {BossyDropdownMenuConfig} from '../dropdown-menu/dropdown-menu.config';
 
 export class BossyDropdownConfig {
   constructor(public name: string,
-              public type: string,
-              public items: Array<BossyDropdownMenuConfig>,
-              public isSplit?: boolean,
-              public size?: string,
-              public isDropup?: boolean,
-              public isRightAligned?: boolean,
-              public variant?: string) {
+              public items: Array<BossyDropdownMenuConfig>) {
   }
 }
