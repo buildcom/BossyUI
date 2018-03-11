80fd7393ce8137e1a2ebf45b6f515e379a1274c2
ZachW628 <ztron628@gmail.com>
2018-03-11 16:37:18 -0700

    Finished popover component, Dynamic adjustment if fields are empty, and ambigous to work with other elements.
    Signed-off-by: ZachW628 <ztron628@gmail.com>

--- src/bossy-ui/components/popover/popover.component.ts
+++ src/bossy-ui/components/popover/popover.component.ts
@@ -13,28 +13,56 @@ export class BossyPopoverComponent implements OnInit {
   @Input() config: BossyPopoverConfig;
 
   constructor(private _eref: ElementRef) {}
+
   hide = false;
   Top = '0px';
   Left = '0px';
+  eventTarget = undefined;
+  Spacing = 34;
+  arrowTop = '34px';
 
   ngOnInit() {
     this.Top = '0px';
     this.Left = '0px';
     this.hide = false;
+    this.eventTarget = undefined;
+    this.Spacing = 34;
+    this.arrowTop = '34px';
   }
 
   @HostListener('document:click', ['$event.target'])
   onClick(event: any) {
+    // This initial if state should only run once and find the popover element assoiciated with and adds an event listener
+    if (this.eventTarget === undefined) {
+      this.calcSpacing();
+      const elements = document.body.querySelectorAll('[id=' + this.config.popoverID + ']');
+      this.eventTarget = elements[0];
+      this.eventTarget.addEventListener('click', this.ShowPopoverOnClick.bind(this));
+      if (this.eventTarget.contains(event)) {
+        this.ShowPopoverOnClick(event);
+      }
+    }
+
     if (this.config.dismissable && this.hide) {
-      if (!this._eref.nativeElement.contains(event)) {
+      if (!this.eventTarget.contains(event)) {
         this.hide = false;
       }
     }
   }
 
   ShowPopoverOnClick(event: any) {
-    this.Top = (event.target.offsetTop - 34) + 'px';
-    this.Left = event.target.offsetLeft + event.target.offsetWidth + 'px';
+    this.Top = (this.eventTarget.offsetTop - this.Spacing) + 'px';
+    this.Left = this.eventTarget.offsetLeft + this.eventTarget.offsetWidth + 'px';
     this.hide = !this.hide;
+  }
+
+  calcSpacing() {
+    if (this.config.popoverData !== '' && this.config.popoverTitle !== '') {
+      this.Spacing = 34;
+      this.arrowTop = '34px';
+    } else {
+      this.Spacing = 0;
+      this.arrowTop = '10px';
+    }
   }
 }
--- src/bossy-ui/components/popover/popover.config.ts
+++ src/bossy-ui/components/popover/popover.config.ts
@@ -3,6 +3,7 @@ export class BossyPopoverConfig {
   public hide: boolean;
   constructor(
               public name: string,
+              public popoverID: string,
               public dismissable: boolean,
               public popoverTitle: string,
               public popoverData: string) {
