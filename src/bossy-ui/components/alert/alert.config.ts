634a2e4d5fe5fbcee65140fb543fcc32e80cd061
jmwilhite <jwilhite4@mail.csuchico.edu>
2018-03-12 12:03:23 -0700

    660: fixed quotation marks

--- src/bossy-ui/components/alert/alert.config.ts
+++ src/bossy-ui/components/alert/alert.config.ts
@@ -6,13 +6,13 @@ export class BossyAlertConfig {
               public large: boolean) {
   }
 }
-export enum alertType{
-    primary= "primary",
-    secondary= "secondary",
-    success= "success",
-    danger= "danger",
-    warning= "warning",
-    info= "info",
-    light= "light",
-    dark= "dark"
+export enum alertType {
+    primary= 'primary',
+    secondary= 'secondary',
+    success= 'success',
+    danger= 'danger',
+    warning= 'warning',
+    info= 'info',
+    light= 'light',
+    dark= 'dark'
 }
