1e75afab417e0e202439564d1206b818a372325a
ZachW628 <ztron628@gmail.com>
2018-03-11 23:48:12 -0700

    finished component may need more error checking
    Signed-off-by: ZachW628 <ztron628@gmail.com>

--- src/bossy-ui/components/popover/popover.component.ts
+++ src/bossy-ui/components/popover/popover.component.ts
@@ -39,14 +39,18 @@ export class BossyPopoverComponent implements OnInit {
       this.calcSpacing();
       const elements = document.body.querySelectorAll('[id=' + this.config.popoverID + ']');
       this.eventTarget = elements[0];
-      this.eventTarget.addEventListener('click', this.ShowPopoverOnClick.bind(this));
-      if (this.eventTarget.contains(event)) {
-        this.ShowPopoverOnClick(event);
+      if (this.eventTarget !== undefined) {
+        this.eventTarget.addEventListener('click', this.ShowPopoverOnClick.bind(this));
+        if (this.eventTarget.contains(event)) {
+          this.ShowPopoverOnClick(event);
+        }
+      } else {
+        console.error('Popover could not resolve element', this.config.popoverID);
       }
     }
 
 
-    if (this.config.dismissable && this.hide) {
+    if (this.eventTarget !== undefined && this.config.dismissable && this.hide) {
       if (!this.eventTarget.contains(event)) {
         this.hide = false;
       }
@@ -55,58 +59,60 @@ export class BossyPopoverComponent implements OnInit {
 
 
   ShowPopoverOnClick(event: any) {
-    if (this.config.placement == 'top') {
-      this.hide = !this.hide;
-      this.cdRef.detectChanges();
-      let popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
-      if (popover_ref != undefined) {
-        this.Top = (this.eventTarget.offsetTop - (popover_ref.offsetHeight)) - (this.Spacing/4) + 'px';
-        this.Left = this.eventTarget.offsetLeft + (this.eventTarget.offsetWidth/2) - (popover_ref.offsetWidth/2)+ 'px';
-        this.arrowTop =  (popover_ref.offsetHeight - 10) + 'px';
-        this.arrowLeft = popover_ref.offsetWidth/2 + 'px';
-        this.rotate = 'rotate(270deg)';
-      }
-    } else if (this.config.placement == 'bottom') {
-      this.hide = !this.hide;
-      this.cdRef.detectChanges();
-      let popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
-      if (popover_ref != undefined) {
-        this.Top = (this.eventTarget.offsetTop + (this.eventTarget.offsetHeight)) + (this.Spacing/4) + 'px';
-        this.Left = this.eventTarget.offsetLeft + (this.eventTarget.offsetWidth/2) - (popover_ref.offsetWidth/2)+ 'px';
-        this.arrowTop = '-16px';
-        this.arrowLeft = popover_ref.offsetWidth/2 + 'px';
-        this.rotate = 'rotate(90deg)';
-      }
-    } else if (this.config.placement == 'left') {
-      this.hide = !this.hide;
-      this.cdRef.detectChanges();
-      let popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
-      if (popover_ref != undefined) {
-        this.Left = (this.eventTarget.offsetLeft - popover_ref.offsetWidth - (this.Spacing/2)) + 'px';
-        this.Top = (this.eventTarget.offsetTop - ((popover_ref.offsetHeight/2) - this.eventTarget.offsetHeight/2)) + 'px';
-        this.arrowTop = popover_ref.offsetHeight/2 - (popover_ref.firstElementChild.offsetHeight/2) + 'px';
-        this.arrowLeft = (popover_ref.offsetWidth - 1) + 'px';
-        this.rotate = 'rotate(180deg)';
-      }
-    } else {
-      if (this.config.placement != 'right') {
-        console.error('invalid placement defaulting to {placement: right}');
-      }
-      this.hide = !this.hide;
-      this.cdRef.detectChanges();
-      let popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
-      if (popover_ref != null) {
-        this.Top = (this.eventTarget.offsetTop - ((popover_ref.offsetHeight/2) - this.eventTarget.offsetHeight/2)) + 'px';
-        this.Left = this.eventTarget.offsetLeft + this.eventTarget.offsetWidth + 'px';
-        this.arrowTop = popover_ref.offsetHeight/2 - (popover_ref.firstElementChild.offsetHeight/2) + 'px';
-        this.arrowLeft = '-8px';
-        this.rotate = 'rotate(0deg)';
+    if (this.eventTarget !== undefined) {
+      if (this.config.placement === 'top') {
+        this.hide = !this.hide;
+        this.cdRef.detectChanges();
+        const popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
+        if (popover_ref !== undefined) {
+          this.Top = (this.eventTarget.offsetTop - (popover_ref.offsetHeight)) - (this.Spacing / 4) + 'px';
+          this.Left = this.eventTarget.offsetLeft + (this.eventTarget.offsetWidth / 2) - (popover_ref.offsetWidth / 2) + 'px';
+          this.arrowTop =  (popover_ref.offsetHeight - 10) + 'px';
+          this.arrowLeft = popover_ref.offsetWidth / 2 + 'px';
+          this.rotate = 'rotate(270deg)';
+        }
+      } else if (this.config.placement === 'bottom') {
+        this.hide = !this.hide;
+        this.cdRef.detectChanges();
+        const popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
+        if (popover_ref !== undefined) {
+          this.Top = (this.eventTarget.offsetTop + (this.eventTarget.offsetHeight)) + (this.Spacing / 4) + 'px';
+          this.Left = this.eventTarget.offsetLeft + (this.eventTarget.offsetWidth / 2) - (popover_ref.offsetWidth / 2) + 'px';
+          this.arrowTop = '-16px';
+          this.arrowLeft = popover_ref.offsetWidth / 2 + 'px';
+          this.rotate = 'rotate(90deg)';
+        }
+      } else if (this.config.placement === 'left') {
+        this.hide = !this.hide;
+        this.cdRef.detectChanges();
+        const popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
+        if (popover_ref !== undefined) {
+          this.Left = (this.eventTarget.offsetLeft - popover_ref.offsetWidth - (this.Spacing / 2 )) + 'px';
+          this.Top = (this.eventTarget.offsetTop - ((popover_ref.offsetHeight / 2) - this.eventTarget.offsetHeight / 2 )) + 'px';
+          this.arrowTop = popover_ref.offsetHeight / 2 - (popover_ref.firstElementChild.offsetHeight / 2) + 'px';
+          this.arrowLeft = (popover_ref.offsetWidth - 1) + 'px';
+          this.rotate = 'rotate(180deg)';
+        }
+      } else {
+        if (this.config.placement !== 'right') {
+          console.error('invalid placement defaulting to {placement: right}');
+        }
+        this.hide = !this.hide;
+        this.cdRef.detectChanges();
+        const popover_ref = this._eref.nativeElement.querySelectorAll('div.popover')[0];
+        if (popover_ref !== undefined) {
+          this.Top = (this.eventTarget.offsetTop - ((popover_ref.offsetHeight / 2) - this.eventTarget.offsetHeight / 2)) + 'px';
+          this.Left = this.eventTarget.offsetLeft + this.eventTarget.offsetWidth + 'px';
+          this.arrowTop = popover_ref.offsetHeight / 2 - (popover_ref.firstElementChild.offsetHeight / 2) + 'px';
+          this.arrowLeft = '-8px';
+          this.rotate = 'rotate(0deg)';
+        }
       }
     }
   }
 
   calcSpacing() {
-    if (this.config.placement == 'top' || this.config.placement == 'bottom') {
+    if (this.config.placement === 'top' || this.config.placement === 'bottom') {
       if (this.config.popoverData !== '' && this.config.popoverTitle !== '') {
         this.Spacing = 34;
         this.arrowTop = '34px';
