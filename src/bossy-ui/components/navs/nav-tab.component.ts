46377af3874f63604eecfa52bbb367f4878ff240
Taber Storm Fitzgerald <tabersays@gmail.com>
2018-05-03 19:16:35 -0700

    test

---
+++ src/bossy-ui/components/navs/nav-tab.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'bossy-nav-tab',
+    templateUrl: './nav-tab.html',
+})
+
+export class BossyNavTabComponent {
+    isActive = false;
+};
