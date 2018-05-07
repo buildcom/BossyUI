347a0f2ae5a384daf2972d47e8386316c2239f48
Unknown <mescutia@mail.csuchico.edu>
2018-05-07 09:19:52 -0700

    Working on popper.

--- src/bossy-ui/components/dropdown/dropdown.component.ts
+++ src/bossy-ui/components/dropdown/dropdown.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
-import {BossyDropdownConfig} from './dropdown.config';
+import { Component, Input, OnInit, SimpleChanges, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { BossyDropdownConfig } from './dropdown.config';
 
 @Component({
   selector: 'bossy-dropdown',
@@ -8,11 +8,21 @@ import {BossyDropdownConfig} from './dropdown.config';
 
 export class BossyDropdownComponent implements OnInit {
   @Input() config: BossyDropdownConfig;
+  @ViewChild('popper') popper: ElementRef;
+  @ViewChild('referemce') reference: ElementRef;
   showMenu = false;
   ariaExpanded = false;
   btnSize = '';
   btnDir = '';
   btnShow = '';
+
+  anotherPopper = new Popper(
+  this.reference,
+  this.popper,
+  {
+    // popper options here
+  }
+);
 
   showMenuOnClick() {
     this.showMenu = !this.showMenu;
@@ -26,25 +36,24 @@ export class BossyDropdownComponent implements OnInit {
   }
 
   ngOnInit() {
-    if ( this.config.btnSize === 'small' ) {
+    if (this.config.btnSize === 'small') {
       this.btnSize = 'btn-sm';
-    } else if ( this.config.btnSize === 'large' ) {
+    } else if (this.config.btnSize === 'large') {
       this.btnSize = 'btn-lg';
     } else {
       this.btnSize = '';
     }
 
-    if ( this.config.direction === 'up') {
+    if (this.config.direction === 'up') {
       this.btnDir = 'dropup';
-    } else if ( this.config.direction === 'down' ) {
+    } else if (this.config.direction === 'down') {
       this.btnDir = 'dropdown';
-    } else if ( this.config.direction === 'left' ) {
+    } else if (this.config.direction === 'left') {
       this.btnDir = 'dropleft';
-    } else if ( this.config.direction === 'right' ) {
+    } else if (this.config.direction === 'right') {
       this.btnDir = 'dropright';
     } else {
       this.btnDir = 'dropdown';
     }
-
   }
 }
