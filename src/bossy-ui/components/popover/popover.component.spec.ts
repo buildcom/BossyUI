22c018b98142469ca824c20642bd9bb1db44bd7d
ZachW628 <ztron628@gmail.com>
2018-05-09 21:22:33 -0700

    Fixed the test so That the coverage dosen't suck and it actually test the functions of the component
    Signed-off-by: ZachW628 <ztron628@gmail.com>

--- src/bossy-ui/components/popover/popover.component.spec.ts
+++ src/bossy-ui/components/popover/popover.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BossyPopoverComponent } from './popover.component';
 import { EventEmitter } from '@angular/core/src/event_emitter';
+import { BossyPopoverConfig } from './popover.config';
+
+
+let SuperConfig: BossyPopoverConfig;
 
 
 describe('the popover should', () => {
@@ -14,45 +18,100 @@ describe('the popover should', () => {
   it('be active', () => {
     const componentFixture = TestBed.createComponent(BossyPopoverComponent);
     const componentInstance = componentFixture.componentInstance;
-    componentInstance.hide = true;
+    SuperConfig = new BossyPopoverConfig('popover',
+                                         'popover1234',
+                                         true,
+                                         'right',
+                                         'PoverOver Title',
+                                         'Popover Description'
+                                        );
+
+    componentInstance.config = SuperConfig;
+    componentInstance.ngOnInit();
     componentFixture.detectChanges();
-    const popoverElement = componentFixture.debugElement.query(e1 => e1.name === 'popover');
+    const buttonClick = componentFixture.debugElement.nativeElement.querySelector('button');
+    componentInstance.ShowPopoverOnClick(buttonClick.click());
+    componentFixture.detectChanges();
+    const popoverElement = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
     expect(popoverElement).toBeTruthy();
   });
 
   it('not be active', () => {
     const componentFixture = TestBed.createComponent(BossyPopoverComponent);
     const componentInstance = componentFixture.componentInstance;
-    componentInstance.hide = false;
-    componentFixture.detectChanges();
-    const popoverElement = componentFixture.debugElement.query(e1 => e1.name === 'popover');
-    expect(popoverElement).toBeFalsy();
+    SuperConfig = new BossyPopoverConfig('popover',
+                                         'popover1234',
+                                          false,
+                                         'left',
+                                         '',
+                                         'Popover Description'
+                                        );
 
+    componentInstance.config = SuperConfig;
+    componentInstance.ngOnInit();
+    componentFixture.detectChanges();
+    const popoverElement = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
+    expect(popoverElement).toBeFalsy();
   });
 
-  it('not be active after dismiss event', () => {
+  it('not be active after click event', () => {
     const componentFixture = TestBed.createComponent(BossyPopoverComponent);
     const componentInstance = componentFixture.componentInstance;
-    componentInstance.hide = true;
-    componentInstance.dismissable = true;
+    SuperConfig = new BossyPopoverConfig('popover',
+                                         'popover1234',
+                                         true,
+                                         'left',
+                                         '',
+                                         'Popover Description'
+                                        );
+
+    componentInstance.config = SuperConfig;
+    componentInstance.ngOnInit();
     componentFixture.detectChanges();
+
     const buttonClick = componentFixture.debugElement.nativeElement.querySelector('button');
-    buttonClick.click();
+    componentInstance.ShowPopoverOnClick(buttonClick.click());
     componentFixture.detectChanges();
-    const popoverElement = componentFixture.debugElement.query(e1 => e1.name === 'popover');
-    expect(popoverElement).toBeFalsy();
+
+    const popoverElement1 = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
+    expect(popoverElement1).toBeTruthy();
+
+    document.dispatchEvent(new MouseEvent('click'));
+    componentFixture.detectChanges();
+
+    const popoverElement2 = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
+    expect(popoverElement2).toBeFalsy();
 
   });
 
-  it('be active after dismiss event', () => {
+  /** */
+  it('be active after click event', () => {
     const componentFixture = TestBed.createComponent(BossyPopoverComponent);
     const componentInstance = componentFixture.componentInstance;
-    componentInstance.hide = true;
+    SuperConfig = new BossyPopoverConfig('popover',
+                                         'popover1234',
+                                         false,
+                                         'top',
+                                         '',
+                                         'Popover Description'
+                                        );
+
+    componentInstance.config = SuperConfig;
+    componentInstance.ngOnInit();
     componentFixture.detectChanges();
+
     const buttonClick = componentFixture.debugElement.nativeElement.querySelector('button');
-    buttonClick.click();
+    componentInstance.ShowPopoverOnClick(buttonClick.click());
     componentFixture.detectChanges();
-    const popoverElement = componentFixture.debugElement.query(e1 => e1.name === 'popover');
-    expect(popoverElement).toBeTruthy();
+
+    const popoverElement1 = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
+    expect(popoverElement1).toBeTruthy();
+
+    document.dispatchEvent(new MouseEvent('click'));
+    componentFixture.detectChanges();
+
+    const popoverElement2 = componentFixture.debugElement.nativeElement.querySelectorAll('div.popover')[0];
+    expect(popoverElement2).toBeTruthy();
   });
+  /**/
 });
